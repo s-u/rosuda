6decba5df2145a72951105dd11cf4d8d4d3c4862
Simon Urbanek <simon.urbanek@r-project.org>
2002-05-08 12:38:08 +0000

    Updated to 0.96a (rel. C508)
    git-svn-id: svn://svn.rforge.net/org/trunk@78 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/ibase/Common.java
+++ rosuda/ibase/Common.java
@@ -16,9 +16,9 @@ public class Common
     public static WindowListener defaultWindowListener=null;
 
     /** application version */
-    public static String Version="0.95k";
+    public static String Version="0.96a";
     /** application release */
-    public static String Release="C425";
+    public static String Release="C508";
     /** application type. so far 0=stand-alone (default, 1=applet - set by Wrapper) */
     public static int AppType=0;
 
