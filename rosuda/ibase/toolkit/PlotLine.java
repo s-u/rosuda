ba4df9c9ead8f933a0e6f573af3b225fb268fe16
Simon Urbanek <simon.urbanek@r-project.org>
2002-07-04 13:58:15 +0000

    Separate coordinate types for X and Y; new type 2=relative graphical coord (-1=min, 1=max)
    git-svn-id: svn://svn.rforge.net/org/trunk@123 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/ibase/toolkit/PlotLine.java
+++ rosuda/ibase/toolkit/PlotLine.java
@@ -16,18 +16,31 @@ public class PlotLine extends PlotObject {
 
     public void drawLine(PoGraSS g, int x1, int y1, int x2, int y2) {
 	g.drawLine(x1,y1,x2,y2);
-	System.out.println("line ("+x1+":"+y1+"-"+x2+":"+y2+"), coord="+coordinates);
+        if (Common.DEBUG>0)
+            System.out.println("line ("+x1+":"+y1+"-"+x2+":"+y2+"), coord="+coordX+"/"+coordY);
     }
 
     public void draw(PoGraSS g) {
 	if (c!=null) c.use(g);
-	if (coordinates==0)
-	    drawLine(g,(int)x1,(int)y1,(int)x2,(int)y2);
-	else {
-	    Axis x=pm.getXAxis();
-	    Axis y=pm.getYAxis();
-	    drawLine(g,x.getValuePos(x1),y.getValuePos(y1),
-		       x.getValuePos(x2),y.getValuePos(y2));
+        int rx1=0,rx2=0,ry1=0,ry2=0;
+        if (coordX==0) { rx1=(int)x1; rx2=(int)x2; };
+        if (coordY==0) { ry1=(int)y1; ry2=(int)y2; };
+        if (coordX>0) {
+            Axis x=pm.getXAxis();
+            if (coordX==1) { rx1=x.getValuePos(x1); rx2=x.getValuePos(x2); };
+            if (coordX==2) {
+                rx1=x.gBegin+(int)(((double)x.gLen)*x1);
+                rx2=x.gBegin+(int)(((double)x.gLen)*x2);
+            };
 	}
+        if (coordY>0) {
+            Axis y=pm.getYAxis();
+            if (coordY==1) { ry1=y.getValuePos(y1); ry2=y.getValuePos(y2); };
+            if (coordY==2) {
+                ry1=y.gBegin+(int)(((double)y.gLen)*y1);
+                ry2=y.gBegin+(int)(((double)y.gLen)*y2);
+            };
+        }
+        drawLine(g,rx1,ry1,rx2,ry2);
     }
 }
--- rosuda/ibase/toolkit/PlotObject.java
+++ rosuda/ibase/toolkit/PlotObject.java
@@ -2,7 +2,7 @@ public class PlotObject {
     PlotManager pm;
     boolean visible=false;
     boolean clip=false;
-    int coordinates=0; // 0=abs-geom, 1=var.space
+    int coordX=0, coordY=0; // 0=abs-geom, 1=var.space, 2=relative geom. (-1=min, 1=max)
 
     public PlotObject(PlotManager p) { pm=p; p.add(this); }
 
@@ -11,6 +11,10 @@ public class PlotObject {
 
     public boolean isVisible() { return visible; }
     public void setVisible(boolean vis) { visible=vis; }
-    public void useVarSpace(boolean vs) { coordinates=vs?1:0; }
+    /** deptrecated! */
+    public void useVarSpace(boolean vs) { coordX=coordY=vs?1:0; }
+    public void setCoordinates(int ct) { coordX=coordY=ct; };
+    public void setCoordinates(int cx, int cy) { coordX=cx; coordY=cy; };
+
     public void setClip(boolean cl) { clip=cl; }
 }
