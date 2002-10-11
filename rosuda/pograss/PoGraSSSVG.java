5b906798eca74a1849fe71203fba1c717b4295b2
Simon Urbanek <simon.urbanek@r-project.org>
2002-10-11 13:40:44 +0000

    Added PoGraSS supporting SVG via batik's svggen
    git-svn-id: svn://svn.rforge.net/org/trunk@199 955e8658-2bef-402f-a597-8b5c18d8b76d

---
+++ rosuda/pograss/PoGraSSSVG.java
@@ -0,0 +1,47 @@
+//
+//  PoGraSSSVG.java
+//  Klimt
+//
+//  Created by Simon Urbanek on Thu Oct 10 2002.
+//  Copyright (c) 2001 __MyCompanyName__. All rights reserved.
+//
+import java.awt.*;
+import java.io.*;
+import org.apache.batik.svggen.SVGGraphics2D;
+import org.apache.batik.dom.GenericDOMImplementation;
+import org.w3c.dom.Document;
+import org.w3c.dom.DOMImplementation;
+
+public class PoGraSSSVG extends PoGraSSgraphics {
+    DOMImplementation domImpl;
+    Document document;
+    SVGGraphics2D svgGenerator;
+    PrintStream ps;
+    
+    public PoGraSSSVG(PrintStream nps, int layer) {
+        super(null,layer);
+        ps=nps;
+        // Get a DOMImplementation
+        domImpl = GenericDOMImplementation.getDOMImplementation();
+        document = domImpl.createDocument(null, "svg", null);
+                // Create an instance of the SVG Generator
+        g=svgGenerator= new SVGGraphics2D(document);
+    };
+
+    public PoGraSSSVG(PrintStream nps) {
+        this(nps,-1);
+    }
+
+    public void begin() { curLayer=0; }
+    
+    public void end() {
+        boolean useCSS = true; // we want to use CSS style attribute
+        try {
+            Writer out = new OutputStreamWriter(ps, "UTF-8");
+            svgGenerator.stream(out, useCSS);
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        };
+    }
+}
