fab6661c5f2aad13942cd0a80c22195d2c2942bf
Simon Urbanek <simon.urbanek@r-project.org>
2002-11-04 10:47:09 +0000

    Added some more doc
    git-svn-id: svn://svn.rforge.net/org/trunk@214 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/JRclient/RFileInputStream.java
+++ rosuda/JRclient/RFileInputStream.java
@@ -1,18 +1,30 @@
 import java.io.*;
 
-
+/** <b>RFileInputStream</b> is an {@link InputStream} to transfer files
+    from <b>Rserve</b> server to the client. It is used very much like
+    a {@link FileInputStream}. Currently mark and seek is not supported.
+    The current implementation is also "one-shot" only, that means the file
+    can be read only once.
+    @version $Id$
+*/
 public class RFileInputStream extends InputStream {
+    /** Rtalk class to use for communication with the Rserve */
     Rtalk rt;
+    /** set to <code>true</code> when {@link #close} was called.
+	Any subsequent read requests on closed stream  result in an 
+	{@link IOException} or error result */
     boolean closed;
+    /** set to <code>true</code> once EOF is reached - or more specifically
+	the first time remore fread returns OK and 0 bytes */
     boolean eof;
 
-    /** tried to open file on the R server, using specified {@link Rtalk} object
+    /** tries to open file on the R server, using specified {@link Rtalk} object
 	and filename. Be aware that the filename has to be specified in host
 	format (which is usually unix). In general you should not use directories
 	since Rserve provides an own directory for every connection. Future Rserve
 	servers may even strip all directory navigation characters for security
 	purposes. Therefore only filenames without path specification are considered
-	valid, the behavior in respect to filenames in undefined. */
+	valid, the behavior in respect to absolute paths in filenames is undefined. */
     RFileInputStream(Rtalk rti, String fn) throws IOException {
 	rt=rti;
 	Rpacket rp=rt.request(Rtalk.CMD_openFile,fn);
@@ -21,12 +33,24 @@ public class RFileInputStream extends InputStream {
 	closed=false; eof=false;
     }
 
+    /** reads one byte from the file. This function should be avoided, since
+	{@link RFileInputStream} provides no buffering. This means that each
+	call to this function leads to a complete packet exchange between
+	the server and the client. Use {@link read(byte[],int,int)} instead
+	whenever possible. In fact this function calls <code>read(b,0,1)</code>.
+	@return -1 on any failure, or the acquired byte (0..255) on success */
     public int read() throws IOException {
 	byte[] b=new byte[1];
 	if (read(b,0,1)<1) return -1;
 	return b[0];
     }
 
+    /** Reads specified number of bytes (or less) from the remote file.
+	@param b buffer to store the read bytes
+	@param off offset where to strat filling the buffer
+	@param len maximal number of bytes to read
+	@return number of bytes read or -1 if EOF reached
+    */
     public int read(byte[] b, int off, int len) throws IOException {
 	if (closed) throw new IOException("File is not open");
 	if (eof) return -1;
@@ -43,6 +67,9 @@ public class RFileInputStream extends InputStream {
 	return rd.length;
     }
 
+    /** close stream - is not related to the actual Rconnection, calling
+	close does not close the Rconnection
+    */
     public void close() throws IOException {
 	Rpacket rp=rt.request(Rtalk.CMD_closeFile,(byte[])null);
 	if (rp==null || !rp.isOk())
