9ccc4c7c4e8d01a3fce61a9624cad0793ff42a89
Simon Urbanek <simon.urbanek@r-project.org>
2002-12-10 19:26:55 +0000

    added support for selected-variable-info appended on the regular query string. [PR]
    git-svn-id: svn://svn.rforge.net/org/trunk@242 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/ibase/toolkit/QueryPopup.java
+++ rosuda/ibase/toolkit/QueryPopup.java
@@ -11,6 +11,7 @@ class QueryPopup
 	int width;
 
 	Window win;
+        SVarSet vs;
 
 	int topMargin=8;
 	int bottomMargin=8;
@@ -19,13 +20,67 @@ class QueryPopup
 
 	public boolean doUpdate=true;
 
-	QPCanvas(Window wn, String ct, int w) {
-	    width=w; win=wn;
-	    setContent(ct); 
+	QPCanvas(Window wn, SVarSet uvs, String ct, int w, int cid) {
+            width=w; win=wn; vs=uvs;
+	    setContent(ct,cid); 
 	}
 
-	public void setContent(String s) {
-	    StringTokenizer st=new StringTokenizer(s,"\n");
+	public void setContent(String t, int cid) {
+            String s=t;
+            if (vs!=null && cid>=0) {
+                int i=0;
+                s+=" \n";
+                while (i<vs.count()) {
+                    if (vs.at(i)!=null && vs.at(i).isSelected()) {
+                        s+=vs.at(i).getName()+": "+vs.at(i).atS(cid)+"\n";
+                    }
+                    i++;
+                }
+            }
+            setContentString(s);
+        }
+
+        public void setContent(String t, int[] cid) {
+            String s=t;
+            if (vs!=null && cid!=null && cid.length>0) {
+                int i=0;
+                s+="\n \n";
+                while (i<vs.count()) {
+                    if (vs.at(i)!=null && vs.at(i).isSelected()) {
+                        if (vs.at(i).isNum()) {
+                            int j=0;
+                            double sum=0.0, ct=0.0, sd=0.0;
+                            while (j<cid.length) {
+                                if (vs.at(i).at(cid[j])!=null) {
+                                    sum+=vs.at(i).atD(cid[j]);
+                                    ct+=1.0;
+                                }
+                                j++;
+                            }
+                            if (ct>0) {
+                                double mean=sum/ct;
+                                j=0;
+                                while (j<cid.length) {
+                                    if (vs.at(i).at(cid[j])!=null) {
+                                        double dif=vs.at(i).atD(cid[j])-mean;
+                                        sd+=dif*dif;
+                                    }
+                                    j++;
+                                }
+                                
+                                s+=vs.at(i).getName()+": mean="+Tools.getDisplayableValue(sum/ct)+" sd="+Tools.getDisplayableValue(Math.sqrt(sd/ct))+"\n";
+                            }
+                        } else {
+                        }
+                    }
+                    i++;
+                }
+            }
+            setContentString(s);
+        }
+
+        void setContentString(String s) {
+            StringTokenizer st=new StringTokenizer(s,"\n");
 	    int tok=st.countTokens();
 	    if (tok<1) {
 		content=new String[1]; content[0]="";
@@ -84,18 +139,31 @@ class QueryPopup
     Window win;
     Window owner;
    
-    public QueryPopup(Frame own, String ct, int w)
+    public QueryPopup(Frame own, SVarSet vs, String ct, int w, int cid)
     {
 	owner=own;
 	win=new Window(own);
-	win.add(cvs=new QPCanvas(win,ct,w));
+	win.add(cvs=new QPCanvas(win,vs,ct,w,cid));
 	cvs.setSize(100,50);
 	win.setBackground(Common.popupColor);	
 	win.pack();
     }
 
+    public QueryPopup(Frame own, SVarSet vs, String ct) {
+        this(own,vs,ct,-1,-1);
+    }
+
+    public void setContent(String s, int[] cid) {
+        // so far we ignore multi-case queries
+        cvs.setContent(s,cid);
+    }
+
+    public void setContent(String s, int cid) {
+	cvs.setContent(s,cid);
+    }
+
     public void setContent(String s) {
-	cvs.setContent(s);
+        cvs.setContent(s,-1);
     }
     
     public void show() {
