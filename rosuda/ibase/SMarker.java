5aaeb883399bd274a400523881a22508df9fdb4b
Simon Urbanek <simon.urbanek@r-project.org>
2003-03-17 09:41:09 +0000

    Added support for color brushing. Marker uses now bit0 for selection and remaining bits for color brushing (secondary mark). API was changed to account for that - notably selection methods use boolean instead of int.
    git-svn-id: svn://svn.rforge.net/org/trunk@274 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/ibase/SMarker.java
+++ rosuda/ibase/SMarker.java
@@ -1,25 +1,5 @@
 import java.util.*;
 
-/*
-class SMarkerOperation {
-    public static const int MOP_SET    = 1;
-    public static const int MOP_CLEAR  = 0;
-    public static const int MOP_SWITCH = 2;
-    
-    int curOp;
-    int curMark;
-    IVector curIV;
-    SMarker m;
-    
-    SMarkerOperation(SMarker M, int mark, int Op, ) {
-	m=M; curMark=mark; curOp=Op; curIV=new IVector();
-    };
-    void public finalize() {
-	
-    };
-};
-*/  
-
 /**
    Maintains a fixed list of markers, i.e. an array of 0..xxx indices that can be marked each.
    It uses both a fixed map and dynamic list of marked positions. Moreover it extends
@@ -34,6 +14,9 @@ public class SMarker extends Notifier implements Commander {
     /** List of marked indices (each as <code>Integer</code> object). */
     Vector list;
 
+    /** highest used mark - for friend functions that need to allocate arrays for mask maps */
+    int maxMark;
+    
     SVarSet masterSet;
     
     /* --- the "currentNode" code is rather experimental because it's not really clean - but still it allows us to use the marker message dispatching to include displaying of splits */    
@@ -46,6 +29,7 @@ public class SMarker extends Notifier implements Commander {
 	list=new Vector();
 	msize=reqsize;
         masterSet=null;
+        maxMark=1; // it is never below 1 since 0 and 1 are used for primary mask
 	//curIV=null; curOp=0;
     };
 
@@ -57,93 +41,101 @@ public class SMarker extends Notifier implements Commander {
 
     /** gets mark at index <code>pos</code>
 	@param pos desired index
-	@return mark at the index <code>pos</code> 
+	@return mark at the index <code>pos</code>. primary mask always returns 1 regardless of secondary mark
     */
-    public int getMark(int pos) { return ((pos<0)||(pos>=msize))?0:mask[pos]; };
+    public int get(int pos) { return ((pos<0)||(pos>=msize))?0:(((mask[pos]&1)==1)?-1:(mask[pos]>>1)); };
+
+    /** gets secondary mask at insex <code>pos</code> */
+    public int getSec(int pos) { return ((pos<0)||(pos>=msize))?0:(mask[pos]>>1); };
 
     /** checks whether index <code>pos</code> has any mark
 	@param pos desired index
-	@return <code>true</code> if there is any mark at the index <code>pos</code> 
+	@return <code>true</code> if there is a primary mark at the index <code>pos</code> 
     */
-    public boolean at(int pos) { return ((pos<0)||(pos>=msize))?false:(mask[pos]!=0); };
+    public boolean at(int pos) { return ((pos<0)||(pos>=msize))?false:((mask[pos]&1)==1); };
 
     /** returs a list of all marked indices
 	@return <code>Vector</code> of <code>Integer</code> objects */
     public Vector getList() { return list; };
     
-    /** sets a mask at specified index
+    /** sets the primary mark at specified index
 	@param pos desired index
-	@param mark to be set (setting mark to 0 does delete any mark if present) */
-    public void set(int pos, int mark) {
-	if ((pos<0)||(pos>=msize)) return;
-	if ((mask[pos]==0)&&(mark!=0)) list.addElement(new Integer(pos));
-	if ((mask[pos]!=0)&&(mark==0)) list.removeElement(new Integer(pos));					   
-	mask[pos]=mark;
-    };
+	@param pMark whether the primary mark is set
+        */
+    public void set(int pos, boolean pMark) {
+	if ((pos<0)||(pos>=msize)) return; // out of range
+        if (pMark==((mask[pos]&1)==1)) return; // no change
+        if (((mask[pos]&1)==0)&&(pMark)) list.addElement(new Integer(pos));
+        if (((mask[pos]&1)==1)&&(!pMark)) list.removeElement(new Integer(pos));
+	mask[pos]^=1;
+    }
 
-    /** modifies mark at specified index according ot the mask supplied (OR)
- 	@param pos desired index
-	@param mark marking-mask - will be binary ORed to the current mark resent, i.e. specified bits are set. */
-    public void setMask(int pos, int mark) {
-	if (mark==0) return;
-	if ((pos<0)||(pos>=msize)) return;
-	if (mask[pos]==0) list.addElement(new Integer(pos));
-	mask[pos]|=mark;
-    };
-    
-    /** modifies mark at specified index according ot the mask supplied (NAND)
- 	@param pos desired index
-	@param mark marking-mask - will be binary NANDed to the current mark resent, i.e. specified bits will be cleared. */
-    public void delMask(int pos, int mark) {
-	if (mark==0) return;
-	if ((pos<0)||(pos>=msize)) return;
-	if (mask[pos]==0) return;
-	mask[pos]&=(-1)^mark;
-	if (mask[pos]==0) list.removeElement(new Integer(pos));
-    };
+    /** sets secondary mark */
+    public void setSec(int pos, int mark) {
+        if (mark>maxMark) maxMark=mark;
+        mark<<=1;
+        mask[pos]=(mask[pos]&1)|mark;
+    }
+
+    /** sets secondary mark of all selected cases */
+    public void setSelected(int mark) {
+        mark<<=1;
+        for(Enumeration e=list.elements(); e.hasMoreElements();) {
+            Integer i=(Integer)e.nextElement();
+            if (i!=null) {
+                int id=i.intValue();
+                mask[id]=(mask[id]&1)|mark;
+            }
+        }
+    }
+
+    public int getMaxMark() { return maxMark; }
     
     /** returns <code>Enumeration</code> of the marked indices.
 	@return <Enumeration> of marked indices (which are a list of <code>Integer</code> objects). Analogous to <code>emelents()</code> method of a <code>Vector</code> */
     public Enumeration elements() { return list.elements(); };
 
-    /** clears entire selection (i.e. sets all marks to 0) */
+    /** clears entire selection (i.e. sets all primary marks to 0) */
     public void selectNone() {
-	int i=0; while(i<msize) { mask[i]=0; i++; };
+	int i=0; while(i<msize) { mask[i]&=(-1)^1; i++; };
 	list.removeAllElements();
     };
 
     /** marks all indices as marked with the specified mark
 	@param mark mark to be used for the selection (passing 0 results in a call to {@link #selectNone} method instead) */
-    public void selectAll(int mark) {
-	if (mark==0) { selectNone(); return; };
+    public void selectAll() {
+        list.removeAllElements();
 	int i=0; 
 	while(i<msize) {
-	    if (mask[i]==0) list.addElement(new Integer(i)); 
-	    mask[i]=mark;
+	    list.addElement(new Integer(i)); 
+	    mask[i]|=1;
 	    i++; 
 	};
     };	
 
+    public void selectInverse() {
+        int i=0;
+        while(i<msize) {
+            if ((mask[i]&1)==0) {
+                list.addElement(new Integer(i));
+                mask[i]|=1;
+            } else {
+                list.removeElement(new Integer(i));
+                mask[i]&=(-1)^1;
+            }
+            i++;
+        };
+    }
+
     public Object run(Object o, String cmd) {
         if (cmd=="selAll") {
-            selectAll(1); NotifyAll(new NotifyMsg(this,Common.NM_MarkerChange));
+            selectAll(); NotifyAll(new NotifyMsg(this,Common.NM_MarkerChange));
         };
         if (cmd=="selNone") {
             selectNone(); NotifyAll(new NotifyMsg(this,Common.NM_MarkerChange));
         }
         if (cmd=="selInv") {
-            int i=0;
-            while(i<msize) {
-                if (mask[i]==0) {
-                    list.addElement(new Integer(i));
-                    mask[i]=1;
-                } else {
-                    list.removeElement(new Integer(i));
-                    mask[i]=0;
-                }
-                i++;
-            };
-            NotifyAll(new NotifyMsg(this,Common.NM_MarkerChange));
+            selectInverse(); NotifyAll(new NotifyMsg(this,Common.NM_MarkerChange));
         }
         return null;
     };
