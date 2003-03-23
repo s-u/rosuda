19c8bd5bac2366a88083972a0edb8ce2bd17eee6
Simon Urbanek <simon.urbanek@r-project.org>
2003-03-23 15:44:37 +0000

    API cleanup (margin not needed)
    git-svn-id: svn://svn.rforge.net/org/trunk@297 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/ibase/toolkit/PlotManager.java
+++ rosuda/ibase/toolkit/PlotManager.java
@@ -1,19 +1,17 @@
 import java.util.*;
 
+/** Plot manager is a container of all plot objects to be painted on the associated PoGraSS canvas.
+*/
 public class PlotManager {
-    int leftm=10, rightm=10, topm=10, botm=10;
     PGSCanvas c;
     Axis ax, ay;
     Vector obj;
 
-    public PlotManager(PGSCanvas cv, Axis a1, Axis a2, int lm, int tm, int rm, int bm) {
-	ax=a1; ay=a2; leftm=lm; rightm=rm; topm=tm; botm=bm;
+    // Warning: we need some some more generic approach: a1/a2 may have changed in the plot after we registered with it
+    public PlotManager(PGSCanvas cv, Axis a1, Axis a2) {
+        c=cv; ax=a1; ay=a2;
 	obj=new Vector();
     }
-
-    public void setMargins(int lm, int tm, int rm, int bm) {
-	leftm=lm; rightm=rm; topm=tm; botm=bm;
-    };
 
     public Axis getXAxis() { return ax; }
     public Axis getYAxis() { return ay; }
@@ -29,5 +27,13 @@ public class PlotManager {
 
     public void add(PlotObject po) {
 	obj.addElement(po);
+    }
+
+    public void rm(PlotObject po) {
+        obj.removeElement(po);
+    }
+    
+    public void update() {
+        c.repaint();
     }
 }
