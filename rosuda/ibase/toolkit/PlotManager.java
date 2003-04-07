2a32e159e56b3ed6f500446755e1d893e7b33f6c
Simon Urbanek <simon.urbanek@r-project.org>
2003-04-07 15:22:36 +0000

    Add the concept of the "current object" and corresponding management functions.
    git-svn-id: svn://svn.rforge.net/org/trunk@321 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/ibase/toolkit/PlotManager.java
+++ rosuda/ibase/toolkit/PlotManager.java
@@ -3,17 +3,28 @@ import java.util.*;
 /** Plot manager is a container of all plot objects to be painted on the associated PoGraSS canvas.
 */
 public class PlotManager {
+    /** parent plot of this manager */
     PGSCanvas c;
+    /** list of PlotObjects managed by this manager */
     Vector obj;
+    /** pointer to the current object int the list */
+    int ptr;
 
+    /** construct a new plot manager for the given plot
+        @param cv parent plot */
     public PlotManager(PGSCanvas cv) {
         c=cv;
 	obj=new Vector();
+        ptr=-1;
     }
 
+    /** get axis object of the plot - simply calls {@link PGSCanvas.getXAxis} */
     public Axis getXAxis() { return c.getXAxis(); }
+
+    /** get axis object of the plot - simply calls {@link PGSCanvas.getYAxis} */
     public Axis getYAxis() { return c.getYAxis(); }
 
+    /** actual painting method */
     public void drawLayer(PoGraSS g,int layer, int layers) {
 	int i=0;
 	while(i<obj.size()) {
@@ -26,10 +37,31 @@ public class PlotManager {
 
     public void add(PlotObject po) {
 	obj.addElement(po);
+        ptr=obj.size()-1; // set current pointer to the newly created object
     }
 
     public void rm(PlotObject po) {
+        if (ptr>=0 && obj.elementAt(ptr)==po) ptr=obj.size()-2;
         obj.removeElement(po);
+    }
+
+    public int getCurrentID() { return ptr; }
+
+    public void setCurrentID(int newID) { ptr=newID; }
+    
+    public PlotObject getCurrentObject() {
+        return (ptr>=0 && ptr<obj.size())?(PlotObject)obj.elementAt(ptr):null;
+    }
+
+    public int setCurrentObject(PlotObject po) {
+        if (obj.size()<1) return -1;
+        int i=0;
+        while(i<obj.size()) {
+            if (obj.elementAt(i)==po)
+                return ptr=i;
+            i++;
+        }
+        return -1;                
     }
     
     public void update(int layer) {
