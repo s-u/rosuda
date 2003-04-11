329b6dc1fe75c8ea02808d04633aa2eec3ab4925
Simon Urbanek <simon.urbanek@r-project.org>
2003-04-11 11:49:38 +0000

    Plugin to check Rserve connection via JRclient and start Rserve if necessary.
    git-svn-id: svn://svn.rforge.net/org/trunk@323 955e8658-2bef-402f-a597-8b5c18d8b76d

---
+++ rosuda/plugins/PluginStartRserve.java
@@ -0,0 +1,42 @@
+//  PluginStartRserve.java
+//  Klimt
+//
+//  Created by Simon Urbanek on Fri Apr 11 2003.
+//  Copyright (c) 2003 Simon Urbanek. All rights reserved.
+//
+
+/** This plugin encapsulates simple communication with Rserve and starts Rserve is the communication fails. this plugin requires JRclient. */
+public class PluginStartRserve extends Plugin {
+    String uRs=null;
+
+    public boolean initPlugin() {
+        if (uRs==null) uRs="R CMD Rserve";
+        return true;
+    }
+
+    public void setParameter(String par, Object val) {
+        if (par=="startCmd") uRs=(String)val;
+    }
+
+    public Object getParameter(String par) {
+        if (par=="startCmd") return uRs;
+        return null;
+    }
+        
+    public boolean execPlugin() {
+        Rconnection rc=new Rconnection();
+        if (!rc.isOk()) {
+            if (Common.DEBUG>0)
+                System.out.println("Rserve is not running, trying to start it ("+uRs+")");
+            try {
+                Runtime.getRuntime().exec(uRs);
+                return true;
+            } catch(Exception rte) {
+                if (Common.DEBUG>0)
+                    System.out.println("Can't start Rserve: "+rte.getMessage());
+                return false;
+            };
+        }
+        return true;
+    }
+}
