124994b746a4c4fc0aa8aaf4f7fb521fdbdb0bb9
Simon Urbanek <simon.urbanek@r-project.org>
2003-04-29 04:46:20 +0000

    Add support for alpha values and #RRGGBBAA
    git-svn-id: svn://svn.rforge.net/org/trunk@326 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/ibase/toolkit/PlotColor.java
+++ rosuda/ibase/toolkit/PlotColor.java
@@ -1,18 +1,23 @@
 public class PlotColor {
-    int r,g,b;
+    int r,g,b,a;
     String nam;
     boolean RGB;
+    boolean alpha;
 
     public PlotColor(int R, int G, int B) {
-	r=R; g=G; b=B; RGB=true;
+        r=R; g=G; b=B; RGB=true; alpha=false;
     }
 
     public PlotColor(String n) {
-	if (n.length()==7 && n.charAt(0)=='#') { // #RRGGBB notation
+	if ((n.length()==7 || n.length()==9) && n.charAt(0)=='#') { // #RRGGBB notation
 	    try {
 		int ir=Integer.parseInt(n.substring(1,3),16);
 		int ig=Integer.parseInt(n.substring(3,5),16);
 		int ib=Integer.parseInt(n.substring(5,7),16);
+                if (n.length()==9) {
+                    a=Integer.parseInt(n.substring(7,9),16);
+                    alpha=true;
+                }
 		r=ir; g=ig; b=ib;
 		RGB=true;
 	    } catch(Exception e) {
@@ -24,13 +29,16 @@ public class PlotColor {
     }
 
     public void use(PoGraSS p) {
-	if (RGB)
-	    p.setColor(r,g,b);
-	else
+        if (RGB) {
+            if (alpha)
+                p.setColor((float)r/255.0F,(float)g/255.0F,(float)b/255.0F,(float)a/255.0F);
+            else
+                p.setColor(r,g,b);
+        } else
 	    p.setColor(nam);
     }
 
     public String toString() {
-        return "PlotColor("+((RGB)?""+r+"/"+g+"/"+b:nam)+")";
+        return "PlotColor("+((RGB)?(""+r+"/"+g+"/"+b+(alpha?"-"+a:"")):nam)+")";
     }
 }
