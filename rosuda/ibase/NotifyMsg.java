6816e6ab285dd75fa43ab79f0de4944210c7a33b
Simon Urbanek <simon.urbanek@r-project.org>
2003-05-11 22:14:13 +0000

    Added command and parameters objects and corresponding accessor methods.
    git-svn-id: svn://svn.rforge.net/org/trunk@340 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/ibase/NotifyMsg.java
+++ rosuda/ibase/NotifyMsg.java
@@ -4,13 +4,23 @@
 public class NotifyMsg {
     Object source;
     int    messageID;
+    String cmd;
+    Object[] par;
+
+    public NotifyMsg(Object src, int msgid, String command, Object[] params) {
+        source=src; messageID=msgid; cmd=command; par=params;	
+    }
+
+    public NotifyMsg(Object src, int msgid, String command) {
+	this(src, msgid, command, null);
+    }
 
     public NotifyMsg(Object src, int msgid) {
-        source=src; messageID=msgid;
-    };
+	this(src, msgid, null, null);
+    }
 
     public NotifyMsg(Object src) {
-        this(src,0);
+        this(src, 0, null, null);
     }
 
     public Object getSource() {
@@ -19,6 +29,34 @@ public class NotifyMsg {
 
     public int getMessageID() {
         return messageID;
+    }
+
+    public String getCommand() {
+	return cmd;
+    }
+
+    public Object[] getParams() {
+	return par;
+    }
+
+    public int parCount() {
+	return (par==null)?0:par.length;
+    }
+
+    public Object parAt(int pos) {
+	return (par==null||pos<0||pos>=par.length)?null:par[pos];
+    }
+
+    public int parI(int pos) {
+	return (par==null||pos<0||pos>=par.length)?0:(((Number)par[pos]).intValue());
+    }
+
+    public double parD(int pos) {
+	return (par==null||pos<0||pos>=par.length)?0:(((Number)par[pos]).doubleValue());
+    }
+
+    public String parS(int pos) {
+	return (par==null||pos<0||pos>=par.length)?null:(par[pos].toString());
     }
 
     public String toString() {
