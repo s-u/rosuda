32fd2400d603f5b643560a582b93a610b1b1470e
Simon Urbanek <simon.urbanek@r-project.org>
2003-05-22 16:10:33 +0000

    Support multiple buttons.
    git-svn-id: svn://svn.rforge.net/org/trunk@357 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/util/MsgDialog.java
+++ rosuda/util/MsgDialog.java
@@ -6,11 +6,20 @@ import java.awt.event.*;
 */
 public class MsgDialog extends Dialog implements ActionListener
 {
+    public String lastResult;
+    public static final String[] okCancel = {"OK","Cancel"};
+    public static final String[] yesNoCancel = {"Yes","No","Cancel"};
+    public static final String[] yesNo = {"Yes","No"};
+    
     /** Creates and displays the dialog box
 	@param par parent frame (or <code>null</code> if none)
  	@param Cap caption title of the dialog box
 	@param Msg message to be displayed */
-    public MsgDialog(Frame par, String Cap, String Msg)
+    public MsgDialog(Frame par, String Cap, String Msg) {
+        this(par,Cap,Msg,null);
+    }
+
+    public MsgDialog(Frame par, String Cap, String Msg, String[] buts)
     {
 	super(par,true);
 	setLayout(new BorderLayout());
@@ -19,12 +28,20 @@ public class MsgDialog extends Dialog implements ActionListener
 	Panel p=new Panel();	
 	p.setLayout(new FlowLayout());
 	Button b;
-	p.add(b=new Button("OK"));
+        if (buts==null || buts.length==0) {
+            p.add(b=new Button("OK"));
+            b.addActionListener(this);
+        } else {
+            int i=0;
+            while (i<buts.length) {
+                p.add(b=new Button(buts[i++]));
+                b.addActionListener(this);
+            }
+        }
 	add(p,"South");
 	add(new SpacingPanel(),"West"); 
 	add(new SpacingPanel(),"East");
         add(new SpacingPanel(50,10),"North");
-	b.addActionListener(this);
 	pack();
 	setResizable(false);
 	//b.setSize(new Dimension(60,20));
@@ -33,6 +50,7 @@ public class MsgDialog extends Dialog implements ActionListener
 
     public void actionPerformed(ActionEvent e)
     {
+        lastResult=e.getActionCommand();
         dispose();
     };
 };
