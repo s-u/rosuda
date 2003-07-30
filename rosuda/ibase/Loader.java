b80c83ba6c9a876a4945fc5856002943a3454198
Simon Urbanek <simon.urbanek@r-project.org>
2003-07-30 22:57:08 +0000

    Loader class will eventually take over functionality of former RTree. Its modular setup should prevent the spaghetti code as in RTree.
    git-svn-id: svn://svn.rforge.net/org/trunk@446 955e8658-2bef-402f-a597-8b5c18d8b76d

---
+++ rosuda/ibase/Loader.java
@@ -0,0 +1,20 @@
+//
+//  Loader.java
+//  Klimt
+//
+//  Created by Simon Urbanek on Wed Jul 30 2003.
+//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+//
+package org.rosuda.ibase;
+
+import java.io.*;
+
+public class Loader {
+    public static int LoadTSV(BufferedReader r, SVarSet vset) {
+        return 0;
+    }
+
+    public static int LoadPolygons(BufferedReader r) {
+        return 0;
+    }
+}
