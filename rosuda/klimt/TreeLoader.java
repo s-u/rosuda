10acdcc79c418b315f1340ee13f9e20718ef79a8
Simon Urbanek <simon.urbanek@r-project.org>
2003-07-30 22:59:16 +0000

    Extension to the generic Loader, supporting trees.
    git-svn-id: svn://svn.rforge.net/org/trunk@450 955e8658-2bef-402f-a597-8b5c18d8b76d

---
+++ rosuda/klimt/TreeLoader.java
@@ -0,0 +1,17 @@
+//
+//  TreeLoader.java
+//  Klimt
+//
+//  Created by Simon Urbanek on Wed Jul 30 2003.
+//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+//
+package org.rosuda.klimt;
+
+import java.io.*;
+import org.rosuda.ibase.*;
+
+public class TreeLoader extends Loader {
+    public static SNode LoadTree(BufferedReader r, SVarSet vset, String name) {
+        return null;
+    }
+}
