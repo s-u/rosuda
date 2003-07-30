1110f443bd752d551b4e958154ad6bfa0dedd8f2
Simon Urbanek <simon.urbanek@r-project.org>
2003-07-30 23:00:53 +0000

    Every root node can have one associated RootInfo object for storing tree-wide properties, such as name, prediction and response variables, formula, ...
    git-svn-id: svn://svn.rforge.net/org/trunk@452 955e8658-2bef-402f-a597-8b5c18d8b76d

---
+++ rosuda/klimt/RootInfo.java
@@ -0,0 +1,30 @@
+//
+//  RootInfo.java
+//  Klimt
+//
+//  Created by Simon Urbanek on Wed Jul 30 2003.
+//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+//
+
+package org.rosuda.klimt;
+
+import org.rosuda.ibase.*;
+import org.rosuda.ibase.toolkit.*;
+
+/** this class encapsulates information associated with a tree which was formerly stored in the root node */
+public class RootInfo {
+    /** classification or regression variable */
+    public SVar response=null;
+    /** classifier or predicted response */
+    public SVar prediction=null;
+
+    /** root-only: name of the tree */
+    public String name=null;
+    /** formula used to grow the tree */
+    public String formula;
+
+    /** associated frame */
+    public TFrame frame;
+
+    
+}
