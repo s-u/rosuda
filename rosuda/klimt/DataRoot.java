ca793434e741c838b88e378d1f4058ab27cc62fc
Simon Urbanek <simon.urbanek@r-project.org>
2003-07-31 02:02:53 +0000

    Added documentation.
    git-svn-id: svn://svn.rforge.net/org/trunk@458 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/klimt/DataRoot.java
+++ rosuda/klimt/DataRoot.java
@@ -12,12 +12,18 @@ package org.rosuda.klimt;
 import java.util.*;
 import org.rosuda.ibase.*;
 
+/** DataRoot is an object associated with each data set. It contains meta information of the data */
 public class DataRoot {
+    /** Underlying dataset */
     SVarSet dataset;
+    /** Associated tree registry with trees which are based on this data */
     TreeRegistry tr;
+    /** Data set type */
     int type;
 
+    /** Dataset type constant: regular dataset */
     public static final int DT_Regular = 0;
+    /** Dataset type constant: forest data. this changes linking behavior */
     public static final int DT_Forest  = 0x100;
     
     public DataRoot(SVarSet ds, TreeRegistry tr, int dataType) {
@@ -26,22 +32,30 @@ public class DataRoot {
         type=dataType;
     }
 
+    /** create a new data root with a new (empty) tree resistry and data type DT_Regular */
     public DataRoot(SVarSet ds) {
         this(ds,new TreeRegistry(),DT_Regular);
     }
 
+    /** retrieves associated tree registry
+        @return tree registry */
     public TreeRegistry getTreeRegistry() {
         return tr;
     }
 
+    /** retrieves associated dataset
+        @return dataset */
     public SVarSet getDataSet() {
         return dataset;
     }
 
+    /** returns data type of this data set */
     public int getDataType() {
         return type;
     }
 
+    /** override data type (e.g. when new meta data was added)
+        @param dataType new data type */
     public void setDataType(int dataType) {
         type=dataType;
     }
