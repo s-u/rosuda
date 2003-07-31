77830f2d196d4ad3283ed4e7ef060a3cfcb46850
Simon Urbanek <simon.urbanek@r-project.org>
2003-07-31 04:00:59 +0000

    Extracted PlotPrimitives to an own file. Made most private properties protected.
    git-svn-id: svn://svn.rforge.net/org/trunk@482 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/ibase/toolkit/BaseCanvas.java
+++ rosuda/ibase/toolkit/BaseCanvas.java
@@ -9,176 +9,72 @@ import org.rosuda.ibase.*;
 import org.rosuda.pograss.*;
 import org.rosuda.util.*;
 
-/** PlotPrimitive provides a generic interfrace to objects on the screen and their linkage to cases. This basic implementation supports rectangles, points and polygons (always just one per instance). A subclass of {@link BaseCanvas} is free to use its own implementation (for example optimized for speed). Default methods of {@link BaseCanvas} use an array of PlotPrimitives to display graphical objects and map them into cases of the underlying dataset. The mapping can be 1:1 (see scatterplot), 1:n (see histograms), m:1 (see maps) or m:n (see faceplots). Please note that PlotPrimitives are geometric objects, that is specified in geometry coorditates (for reasons of speed).<p>Note: current implementation checks the contents in the sequence: rectangle, point, polygon. First non-null object is taken. Marking is based on the {@link #ref} array which contains case IDs for each case corresponding to the PlotPrimitive. In x:1 case the array is simply of the length 1. Currently there is no specific constructor, meaning that every newly allocated PlotPrimitive is empty. */
-class PlotPrimitive {
-    Rectangle r;
-    Polygon pg;
-    Point pt;
-    Color col;
-    
-    /** references to cases represented by this primitive */
-    int[] ref;
-
-    public String toString() {
-        return "PlotPrimitive["+r+"/"+pt+"/"+pg+"]-"+ref;
-    }
-
-    /** checks whether the PlotPrimitive contains (or in case of a point primitive equals to) the given point.*/
-    public boolean contains(int x, int y) {
-        if (r!=null) return r.contains(x,y);
-        if (pt!=null) return (pt.getX()==x && pt.getY()==y);
-        if (pg!=null) return pg.contains(x,y);
-        return false;
-    }
-
-    /** checks whether the PlotPrimitive intersects (or is contained) in the given rectangle. */
-    public boolean intersects(Rectangle rt) {
-        if (r!=null) return r.intersects(rt);
-        if (pt!=null) return rt.contains(pt);
-        if (pg!=null) return pg.intersects(rt);
-        return false;
-    }
-
-    /** sets mark for cases represented by this PlotPrimitive in following fashion: if the case is already marked then its mark is set to setTo, otherwise the mark is set to 1. */ 
-    public void setMark(SMarker m, boolean setTo) {
-        if (ref!=null) {
-            int j=0, pts=ref.length;
-            while (j<pts) {
-                m.set(ref[j],m.at(ref[j])?setTo:true);
-                j++;
-            }
-        }
-    }
-    
-    /** calculates the proportion of cases with the given mark in relation to total population of cases represented by this PlotPrimitive. The value ranges between 0.0 (no cases with such mark) to 1.0 (all cases with that mark) */
-    public double getMarkedProportion(SMarker m, int mark) {
-        Stopwatch sw=new Stopwatch();
-        if (ref!=null) {
-            int j=0, pts=ref.length;
-            if (pts==0) return 0d;
-            int sc=0;
-            while (j<pts) {
-                if (m.get(ref[j])==mark) sc++;
-                j++;
-            }
-            sw.profile("PlotPrimitive.getMarkedProportion");
-            return ((double)sc)/((double)pts);
-        }
-        return 0d;
-    }
-
-    public int[] getMarkedList(SMarker m) { // [total][count mark 0][mark][count][mark][count]...
-        if (m==null) return null;
-        if (ref!=null) {
-            if (ref.length<1) return null;
-            if (ref.length==1) {
-                int mark=m.get(ref[0]);
-                int[] lst=new int[(mark!=0)?4:2];
-                lst[0]=1;
-                lst[1]=(mark==0)?1:0;
-                if (mark!=0) {
-                    lst[2]=mark;
-                    lst[3]=1;
-                }
-                return lst;
-            }
-            int pts=ref.length;
-            int j=0;
-            int[] cts=new int[m.getMaxMark()+2];
-            while (j<pts) {
-                int mark=m.get(ref[j]);
-                mark++;
-                if (mark<cts.length) cts[mark]++;
-                j++;
-            }
-            int ums=0;
-            j=2;
-            while(j<cts.length) { if (cts[j++]>0) ums++; };
-            int lst[]=new int[3+(ums*2)];
-            lst[0]=pts;
-            lst[1]=cts[1];
-            j=0;
-            int k=2;
-            while(j<cts.length) {
-                if (j!=1 && cts[j]>0) {
-                    lst[k++]=j-1; lst[k++]=cts[j];
-                }
-                j++;
-            }
-            return lst;
-        }
-        return null;
-    }
-    
-    public Color getColor() { return col; }
-}
-
 /** BaseCanvas - basis for all interactive plots which rely on plot primitives concept. To ensure consistent behavior all plots should be based on this class whenever possible. BaseCanvas includes key and mouse handling, selection and queries.<p>Although BaseCanvas is not abstract, is it not usable on its own (except maybe for testing). Any subclasses should override at least the {@link #updateObjects} method to initialize plot primitives. Displaying and selection of following plot primitives is supported out-of-the-box: points, rectangles and polygons. The subclass constructor should (beside calling super constructor) set any of the control flags to customize the behavior of this class.<p>BaseCanvas implements all key and mouse listeners as well as commander interface (actions are mapped into commands). This implies that a subclass it free to overload any individual methods of those. Just make sure that you provide calls to parent methods to preserve all functionality.
 
     @version $Id$
 */
-class BaseCanvas extends PGSCanvas implements Dependent, MouseListener, MouseMotionListener, KeyListener, ActionListener
+public class BaseCanvas extends PGSCanvas implements Dependent, MouseListener, MouseMotionListener, KeyListener, ActionListener
 {
     /** query popup window */
-    QueryPopup qi;
+    protected QueryPopup qi;
 
     /** plot primitives which form the basis for data display and selection */
-    PlotPrimitive[] pp;
+    protected PlotPrimitive[] pp;
 
     /** optional axis. Axis is not initialized by BaseCanvas (since BaseCanvas knows nothing of variables). They are provided to support some degree of consistence among plots which use one or two axes. If used, axis geometry is updated upon resize by the default paint routine and value range is changed by default zooming methods (atm for axes of type T_Num only). The axes are defined with respect to the data, NOT to the geometry. Calling the default {@link #rotate} method results in updated geometry of the axes, meaning that ax does not necessarily have the orientation O_X (unless rotation is prohibited). */
 
     /** by default BaseCanvas caches layers whenever possible, that is only {@link #paintInit} is guaranteed        to be run before {@link #paintBack}, {@link #paintObjects} and {@link #paintSelected}. This implies that none of the later three can rely on anything happening in the preceeding paint methods. This is not the usual behavior of PoGraSS. Setting this dontCache flag to <code>true</code> will enforce the defined PoGraSS behavior which means that all paint.. parts are called in the specified order, no matter which layer is being updated. */
-    boolean dontCache=false;
+    protected boolean dontCache=false;
 
     /** marker of the plot. This marker is used for linked highlighting. */
-    SMarker m;
+    protected SMarker m;
 
-    /** plot area margins. Axes are set in a way to always respect those margins. They default to 0 for BaseCanvas. */ 
-    int mLeft, mRight, mTop, mBottom;
+    /** plot area margins. Axes are set in a way to always respect those margins. They default to 0 for BaseCanvas. */
+    protected int mLeft, mRight, mTop, mBottom;
     /** orientation of the plot. Value between 0 and 3. Increasing orientation results in plot rotating by 90 degrees clock-wise. */
-    int orientation=0;
+    protected int orientation=0;
     /** if set to <code>true</code> then rotating the canvas also results in flipped size geometry of the canvas and re-packed parent frame. */
-    boolean resizeOnRotate=true;
+    protected boolean resizeOnRotate=true;
     /** if set to <code>false</code> then rotating is allowed only between orientation values 0 and 1 (ergo rotation over 180 degrees is not allowed) */
-    boolean allow180=false;
+    protected boolean allow180=false;
     /** if set to <code>true</code> then zoom is allowed */
-    boolean allowZoom=true;
+    protected boolean allowZoom=true;
     /** if set to <code>true</code> then zoom-dragging is allowed. Otherwise stepwise zoom by clicing is the only available zoom method. */
-    boolean allowDragZoom=true;
+    protected boolean allowDragZoom=true;
     /** if set to <code>true</code> then zooming-in always retians the aspect ratio (at least default zoom handling via axes does. If you implement a subclass providing its own zooming features you should honor this flag where applicable) */
-    boolean zoomRetainsAspect=true;
+    protected boolean zoomRetainsAspect=true;
     
     /** run-time flag is set to <code>true</code> if baseDragging is in process */
-    boolean baseDrag;
+    protected boolean baseDrag;
     /** run-time flag is set to <code>true</code> if the current baseDrag is a selection */
-    boolean selDrag; // current baseDrag is selection baseDrag
+    protected boolean selDrag; // current baseDrag is selection baseDrag
     /** run-tim flag is set to <code>true</code> if the current baseDrag is a zoom */
-    boolean zoomDrag;
+    protected boolean zoomDrag;
 
     /** run-time flag is set to <code>true</code> if query mode is on - (currently it means that <Alt> is held down) */
-    boolean inQuery=false;
+    protected boolean inQuery=false;
     /** run-time flag is set to <code>true</code> if zoom mode is on */
-    boolean inZoom=false;
+    protected boolean inZoom=false;
 
     /** if set to <code>true</code> all notifications are rejected. Any subclass is free to use it, BaseCanvas modifies this flag in default zoom processing methods to prevent partial updates when ax and ay are updated sequentially. Any method changing this flag should always restore the state of the flag after it finishes! Also use with care in multi-threaded applications to prevent deadlocks. */
-    boolean ignoreNotifications=false;
+    protected boolean ignoreNotifications=false;
 
     /** this vector can be used to track the sequence of zooms. Zoom out should return to the state before last zoom in (if sensible in the context of the given plot). Any implementation of {@link #performZoomIn} and {@link #performZoomOut} is free to use this vector in any way which suits the implementation.<p>The current default implementation uses pairs of {@link ZoomDescriptorComponent} objects to store status of {@link #ax} and {@link #ay} axes. The vector is automatically initilized to an empty vector by the base constructor. */
-    Vector zoomSequence;
-    
-    int W,H;
-    
-    int baseDragX1,baseDragX2,baseDragY1,baseDragY2;
+    protected Vector zoomSequence;
+
+    protected int W,H;
+
+    protected int baseDragX1,baseDragX2,baseDragY1,baseDragY2;
 
     /** if set to <code>true</code> outline of plot primitives is painted (applies to default paint handler. subclasses of BaseCanvas don't have to respect this flag if may display more complex primitives) */
-    boolean paintOutline=true;
+    protected boolean paintOutline=true;
     /** same functionality as {@link #paintOutline} but applies to selected objects only */
-    boolean selectedPaintOutline=true;
+    protected boolean selectedPaintOutline=true;
     /** if set to <code>true</code> plot primitives are filled. */
-    boolean fillInside=true;
+    protected boolean fillInside=true;
 
     /** if set to <code>true</code> then next repaint will force update of geometry, that is it will behave as if the canvas size was changed resulting in updated axes and objects. {@link #paintPoGraSS} resets this flag to <code>false</code> after calling {@link #updateObjects} and setting everything up. */
-    boolean updateGeometry=false;
+    protected boolean updateGeometry=false;
 
     /** basic constructor. Every subclass must call this constructor
         @param f frame owning this canvas. since BaseCanvas itself doesn't modify any attribute of the frame except for title it is possible to put more canvases into one frame. This doesn't have to hold for subclasses, especially those providing their own menus.
@@ -195,7 +91,7 @@ class BaseCanvas extends PGSCanvas implements Dependent, MouseListener, MouseMot
 	addMouseMotionListener(this);
         addKeyListener(this);
         f.addKeyListener(this);
-        qi=new QueryPopup(f,mark==null?null:mark.masterSet,"BaseCanvas");
+        qi=new QueryPopup(f,mark==null?null:mark.getMasterSet(),"BaseCanvas");
     };
 
     /** notification handler - rebuild objects if necessary (AxisDataChange/VarChange) and repaint */
