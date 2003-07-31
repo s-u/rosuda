b00cb053c5b72bb5c2da101a9e97538f176741d2
Simon Urbanek <simon.urbanek@r-project.org>
2003-07-31 04:06:40 +0000

    Move to org.rosuda structure; Move MapSegmentEntry to an own file and axis transformation methods to org.rosuda.ibase.toolkit.MapSegmentTools
    git-svn-id: svn://svn.rforge.net/org/trunk@489 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/ibase/MapSegment.java
+++ rosuda/ibase/MapSegment.java
@@ -6,36 +6,10 @@
 //  $Id$
 //
 
+package org.rosuda.ibase;
+
 import java.util.Vector;
-
-class MapSegmentEntry {
-    public double xp[], yp[];
-    public boolean isLake;
-    public boolean hasBorder;
-
-    public double minX, maxX, minY, maxY;
-    
-    public MapSegmentEntry() {
-        isLake=false;
-        hasBorder=true;
-    }
-
-    public MapSegmentEntry(double[] xpt, double[] ypt, boolean lake, boolean border) {
-        isLake=lake; hasBorder=border;
-        xp=new double[xpt.length]; yp=new double[xpt.length];
-        int i=0;
-        while(i<xpt.length) {
-            xp[i]=xpt[i];
-            yp[i]=(i<ypt.length)?ypt[i]:0;
-            if (i==0) { minX=maxX=xpt[0]; minY=maxY=ypt[0]; };
-            if (xpt[i]<minX) minX=xpt[i];
-            if (xpt[i]>maxX) maxX=xpt[i];
-            if (ypt[i]<minY) minY=ypt[i];
-            if (ypt[i]>maxY) maxY=ypt[i];
-            i++;
-        };
-    }
-}
+import org.rosuda.util.Tools;
 
 public class MapSegment {
     Vector ents;
@@ -117,25 +91,7 @@ public class MapSegment {
         while (i<mse.yp.length) { ys[i]=poff+(int)((mse.yp[i]+off)*scale); i++; };
         return ys;
     }
-
-    public int[] transViaAxisX(int id, Axis a) {
-        MapSegmentEntry mse=(MapSegmentEntry)ents.elementAt(id);
-        if (mse==null) return null;
-        int[] xs=new int[mse.xp.length];
-        int i=0;
-        while (i<mse.xp.length) { xs[i]=a.getValuePos(mse.xp[i]); i++; };
-        return xs;
-    }
-
-    public int[] transViaAxisY(int id, Axis a) {
-        MapSegmentEntry mse=(MapSegmentEntry)ents.elementAt(id);
-        if (mse==null) return null;
-        int[] ys=new int[mse.yp.length];
-        int i=0;
-        while (i<mse.yp.length) { ys[i]=a.getValuePos(mse.yp[i]); i++; };
-        return ys;
-    }
-
+    
     public String toString() {
         return "Map segment ("+Tools.getDisplayableValue(minX)+","+Tools.getDisplayableValue(minY)+
         ")-("+Tools.getDisplayableValue(maxX)+","+Tools.getDisplayableValue(maxY)+
