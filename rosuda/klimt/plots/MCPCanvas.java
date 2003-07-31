05b6d546c7520d26e6a7bed9c522a18eed19c612
Simon Urbanek <simon.urbanek@r-project.org>
2003-07-31 03:50:08 +0000

    Adapt to the org.rosuda structure.
    git-svn-id: svn://svn.rforge.net/org/trunk@471 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/klimt/plots/MCPCanvas.java
+++ rosuda/klimt/plots/MCPCanvas.java
@@ -1,7 +1,15 @@
+package org.rosuda.klimt.plots;
+
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import java.io.*;
+
+import org.rosuda.ibase.*;
+import org.rosuda.ibase.toolkit.*;
+import org.rosuda.pograss.*;
+import org.rosuda.util.*;
+import org.rosuda.klimt.*;
 
 /** implementation of misclassification plot.
     @version $Id$
@@ -9,7 +17,7 @@ import java.io.*;
 public class MCPCanvas extends PGSCanvas implements Dependent, MouseListener, MouseMotionListener, KeyListener, ActionListener, Commander
 {
     /** associated tree manager */
-    RTree tm;
+    TreeRegistry tm;
     /** associated marker */
     SMarker m;
 
@@ -38,7 +46,7 @@ public class MCPCanvas extends PGSCanvas implements Dependent, MouseListener, Mo
 	@param var source variable
 	@param mark associated marker
     */
-    public MCPCanvas(Frame f,RTree tman, SMarker mark) {
+    public MCPCanvas(Frame f,TreeRegistry tman, SMarker mark) {
         tm=tman; m=mark; setFrame(f); setTitle("MCP"); m.addDepend(this);
 	setBackground(Common.backgroundColor);
 	addMouseListener(this);
@@ -48,7 +56,7 @@ public class MCPCanvas extends PGSCanvas implements Dependent, MouseListener, Mo
 	String myMenu[]={"+","File","~File.Graph","~Edit","+","View","Toggle mode","rotate","~Window","0"};
 	EzMenu.getEzMenu(f,this,myMenu);
         updateBoxes();
-        qi=new QueryPopup(f,(mark==null)?null:mark.masterSet,"MC-plot");
+        qi=new QueryPopup(f,(mark==null)?null:mark.getMasterSet(),"MC-plot");
     };
 
     public void Notifying(NotifyMsg msg, Object o, Vector path) {
@@ -59,10 +67,10 @@ public class MCPCanvas extends PGSCanvas implements Dependent, MouseListener, Mo
     public Dimension getMinimumSize() { return new Dimension(leftm+rightm+10,topm+botm+10); };
 
     void updateBoxes() {
-        Vector v=tm.getTrees();
+        Vector v=tm.getTreeEntries();
         if (v==null) return;
         int bs=v.size();
-        if (Common.DEBUG>0)
+        if (Global.DEBUG>0)
             System.out.println("bs="+bs);
         if (bs==0) return;
         count=new int[bs];
@@ -71,9 +79,10 @@ public class MCPCanvas extends PGSCanvas implements Dependent, MouseListener, Mo
         xv=xr=0;
         SVar r=null;
         while(i<bs) {
-            SNode n=(SNode)v.elementAt(i);
-            if (r==null) r=n.response;
-            SVar c=n.prediction;
+            TreeEntry te=(TreeEntry)v.elementAt(i);
+            SNode n=te.root;
+            if (r==null) r=n.getRootInfo().response;
+            SVar c=n.getRootInfo().prediction;
             if (c!=null) {
                 int j=0;
                 while (j<c.size()) {
@@ -88,7 +97,7 @@ public class MCPCanvas extends PGSCanvas implements Dependent, MouseListener, Mo
 	    }
             if (count[i]>xv) xv=count[i];
             if (c!=null && c.size()-count[i]+xv>xr) xr=c.size()-count[i]+xv;
-            if (Common.DEBUG>0)
+            if (Global.DEBUG>0)
                 System.out.println("i="+i+", xv="+xv+", count="+count[i]+", mark="+mark[i]);
             i++;
         }
@@ -108,20 +117,20 @@ public class MCPCanvas extends PGSCanvas implements Dependent, MouseListener, Mo
         g.defineColor("marked",128,255,128);
         g.defineColor("red",255,0,0);
 
-        Vector v=tm.getTrees();
-
         if (!xdisp) {
             g.setColor("outline");
             g.drawLine(leftm,h-botm,leftm,topm);
             g.drawLine(leftm,h-botm,w-rightm,h-botm);
 
             if (count!=null) { // misclass-only-view
+                Vector v=tm.getTreeEntries();
                 int bs=count.length;
                 double ddx=((double)(rdx-bs*3))/((double)bs);
                 double yf=((double)rdy)/((double)xv);
                 int i=0;
                 while(i<bs) {
-                    SNode r=(SNode)v.elementAt(i);
+                    TreeEntry te=(TreeEntry)v.elementAt(i);
+                    SNode r=te.root;
                     int x1=leftm+(int)(ddx*i)+i*3;
                     g.setColor("fill");
                     int ht=(int)(yf*count[i]);
@@ -133,7 +142,7 @@ public class MCPCanvas extends PGSCanvas implements Dependent, MouseListener, Mo
                     };
                     g.setColor("outline");
                     g.drawRect(x1,h-botm-ht,(int)ddx,ht);
-                    g.drawString(Common.getTriGraph(r.name),x1,h-botm/2);
+                    g.drawString(Common.getTriGraph(r.getRootInfo().name),x1,h-botm/2);
                     i++;
                 };
             };
@@ -179,7 +188,7 @@ public class MCPCanvas extends PGSCanvas implements Dependent, MouseListener, Mo
         int w=Dsize.width, h=Dsize.height;
         int rdx=w-leftm-rightm;
         int rdy=h-botm-topm;
-        Vector v=tm.getTrees();
+        Vector v=tm.getTreeEntries();
         if (v==null) return;
 
         boolean hideQI=true;
@@ -191,17 +200,17 @@ public class MCPCanvas extends PGSCanvas implements Dependent, MouseListener, Mo
             if (!ev.isShiftDown()) m.selectNone();
             i=0;
             SVar r=null;
-            Vector tv=tm.getTrees();
             while(i<bs) {
                 int x1=leftm+(int)(ddx*i)+i*3;
                 int ht=(int)(yf*count[i]);
                 if (ht<10) ht=10;
                 if (x>=x1&&x<x1+(int)ddx&&y<=h-botm&&y>=h-botm-ht) {
-                    SNode rt=(SNode)tv.elementAt(i);
+                    TreeEntry te=(TreeEntry)v.elementAt(i);
+                    SNode rt=te.root;
                     if (Common.isQueryTrigger(ev)) {
-                        String qs="Tree "+rt.name+"\nMiscl. "+count[i]+" ("+mark[i]+" sel.)";
+                        String qs="Tree "+rt.getRootInfo().name+"\nMiscl. "+count[i]+" ("+mark[i]+" sel.)";
                         if (ev.isShiftDown()) {
-                            qs="Tree "+rt.name+" ("+i+")\nMisclassified:\n   "+count[i]+" of "+m.size()+" ("+
+                            qs="Tree "+rt.getRootInfo().name+" ("+i+")\nMisclassified:\n   "+count[i]+" of "+m.size()+" ("+
                             Tools.getDisplayableValue(100.0*((double)count[i])/((double)m.size()),2)+
                             "%)\nSelected:\n  "+mark[i]+" ("+
                             Tools.getDisplayableValue(100.0*((double)mark[i])/((double)count[i]),2)+
@@ -212,10 +221,9 @@ public class MCPCanvas extends PGSCanvas implements Dependent, MouseListener, Mo
                         qi.setLocation(cl.x+x,cl.y+y);
                         qi.show(); hideQI=false;                    
                     } else {
-                        effect=true;
-                        SNode n=(SNode)v.elementAt(i);
-                        if (r==null) r=n.response;
-                        SVar c=n.prediction;
+                        SNode n=te.root;
+                        if (r==null) r=n.getRootInfo().response;
+                        SVar c=n.getRootInfo().prediction;
                         if (c!=null) {
                             int j=0;
                             while (j<c.size()) {
