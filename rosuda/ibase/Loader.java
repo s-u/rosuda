e2120d8674b6790093bd02d77432653cde88ea45
Simon Urbanek <simon.urbanek@r-project.org>
2003-10-07 09:51:37 +0000

    Implemented very basic TSV loader (strings only).
    git-svn-id: svn://svn.rforge.net/org/trunk@545 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/ibase/Loader.java
+++ rosuda/ibase/Loader.java
@@ -8,9 +8,37 @@
 package org.rosuda.ibase;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Loader {
     public static int LoadTSV(BufferedReader r, SVarSet vset) {
+        try {
+            int vsb=vset.count();
+            String s=r.readLine(); // read line
+            StringTokenizer st=new StringTokenizer(s,"\t");
+            while (st.hasMoreTokens()) {
+                String t=st.nextToken();
+                SVar v=new SVar(t);
+                vset.add(v);
+            }
+            int j=0;
+            while (r.ready()) {
+                String ls=r.readLine(); // read line
+                if (ls==null || ls.length()==0) break;
+                StringTokenizer lst=new StringTokenizer(ls,"\t");
+                int i=0;
+                while (lst.hasMoreTokens()) {
+                    String t=lst.nextToken();
+                    SVar v=vset.at(vsb+i);
+                    v.add(t);
+                    i++;
+                }
+                j++;
+            }
+            return j;
+        } catch(IOException e) {
+            System.out.println("Loader.LoadTSV[IOException]: "+e.getMessage());
+        }
         return 0;
     }
 
