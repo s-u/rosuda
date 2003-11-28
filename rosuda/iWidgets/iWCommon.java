9d8d634980479c7e1a8397661999a9cb4873c600
Simon Urbanek <simon.urbanek@r-project.org>
2003-11-28 07:28:36 +0000

    Use proper events of the category External
    git-svn-id: svn://svn.rforge.net/org/trunk@627 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/iWidgets/iWCommon.java
+++ rosuda/iWidgets/iWCommon.java
@@ -8,14 +8,16 @@
 
 package org.rosuda.iWidgets;
 
+import org.rosuda.ibase.*;
+
 public class iWCommon
 {
 	//Constant ints for each type of event used by iWidgets
-	public static int BUTTON_EVENT 		= 101;
-	public static int TICKBOX_EVENT 	= 102;
-	public static int RADIOBUTTON_EVENT = 103;
-	public static int SLIDER_EVENT 		= 104;
-	public static int TEXTBOX_EVENT 	= 105;
+	public static int BUTTON_EVENT 		= Common.NM_ExtEvent + 101;
+	public static int TICKBOX_EVENT 	= Common.NM_ExtEvent + 102;
+	public static int RADIOBUTTON_EVENT     = Common.NM_ExtEvent + 103;
+	public static int SLIDER_EVENT 		= Common.NM_ExtEvent + 104;
+	public static int TEXTBOX_EVENT 	= Common.NM_ExtEvent + 105;
 	
 	// Probably won't need these...
 	public static int getButton_Event()
