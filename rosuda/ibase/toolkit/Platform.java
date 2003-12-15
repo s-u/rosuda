00499d1ba27b5d073bca1035736ae74c8444d6c4
Simon Urbanek <simon.urbanek@r-project.org>
2003-12-15 15:17:12 +0000

    Default handling of the preferences and about box (platform independent).
    git-svn-id: svn://svn.rforge.net/org/trunk@640 955e8658-2bef-402f-a597-8b5c18d8b76d

---
+++ rosuda/ibase/toolkit/Platform.java
@@ -0,0 +1,27 @@
+//
+//  Platform.java
+//
+//  Created by Simon Urbanek on Wed Jul 30 2003.
+//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+//
+
+package org.rosuda.ibase.toolkit;
+
+import java.io.*;
+import java.awt.*;
+import org.rosuda.ibase.*;
+import org.rosuda.ibase.toolkit.*;
+import org.rosuda.util.*;
+
+/** This platform implementation adds a preferences handler for colors and a default about splash screen.
+Use initPlatform("org.rosuda.ibase.toolkit.") in order to enable it.
+*/
+public class Platform extends org.rosuda.util.Platform {
+    public void handleAbout() {
+        SplashScreen.runMainAsAbout("About "+Common.appName);
+    }
+    
+    public void handlePrefs() {
+        PrefsColorFrame.showPrefsDialog();
+    }
+}
