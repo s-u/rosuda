f1629b78fe2047e050b5227f3674f16557138ee9
Simon Urbanek <simon.urbanek@r-project.org>
2003-12-15 15:10:26 +0000

    Default platform definitions for iPlots
    git-svn-id: svn://svn.rforge.net/org/trunk@639 955e8658-2bef-402f-a597-8b5c18d8b76d

---
+++ rosuda/iplots/Platform.java
@@ -0,0 +1,25 @@
+//
+//  PlatformMac.java
+//  Klimt
+//
+//  Created by Simon Urbanek on Wed Jul 30 2003.
+//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+//
+
+package org.rosuda.iplots;
+
+import java.io.*;
+import java.awt.*;
+import org.rosuda.ibase.*;
+import org.rosuda.ibase.toolkit.*;
+import org.rosuda.util.*;
+
+public class Platform extends org.rosuda.util.Platform {
+    public void handleAbout() {
+        SplashScreen.runMainAsAbout("iPlots");
+    }
+
+    public void handlePrefs() {
+        PrefsColorFrame.showPrefsDialog();
+    }
+}
---
+++ rosuda/iplots/PlatformMac.java
@@ -0,0 +1,24 @@
+//
+//  PlatformMac.java
+//
+//  Created by Simon Urbanek on Wed Jul 30 2003.
+//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+//
+
+package org.rosuda.iplots;
+
+import java.io.*;
+import java.awt.*;
+import org.rosuda.ibase.*;
+import org.rosuda.ibase.toolkit.*;
+import org.rosuda.util.*;
+
+public class PlatformMac extends org.rosuda.util.PlatformMac {
+    public void handleAbout() {
+        SplashScreen.runMainAsAbout("About iPlots");
+    }
+
+    public void handlePrefs() {
+        PrefsColorFrame.showPrefsDialog();
+    }
+}
