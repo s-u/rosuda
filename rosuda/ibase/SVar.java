18717593e1a11cccd84e44adc8e6506f64376c5d
Markus Helbig <info@markushelbig.de>
2004-01-06 15:07:55 +0000

    added methods replace for int and double
    git-svn-id: svn://svn.rforge.net/org/trunk@653 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/ibase/SVar.java
+++ rosuda/ibase/SVar.java
@@ -206,7 +206,9 @@ public abstract class SVar extends Notifier
 
     /** replaces an element at specified position
         @returns <code>false</code> if some error occured (overflow, wrong type, ...) */
-    public abstract boolean replace(int i, Object o);
+    public abstract boolean replace(Object o, int index);
+    public boolean replace(double d, int index) { return replace(new Double(d), index); }
+    public boolean replace(int i, int index) { return replace(new Integer(i), index); }
 
     public double getMin() { return min; }
     public double getMax() { return max; }
