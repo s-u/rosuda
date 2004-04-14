c4844f5cfa6db92d444796cb601226b96403693e
Simon Urbanek <simon.urbanek@r-project.org>
2004-04-14 17:32:15 +0000

    Added SEXP2l/l2SEXP, SEXPL array methods and isntallString
    git-svn-id: svn://svn.rforge.net/org/trunk@761 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/JRI/src/jri.h
+++ rosuda/JRI/src/jri.h
@@ -3,6 +3,7 @@
 
 #include <jni.h>
 #include <R.h>
+#include <Rinternals.h>
 #include <Rdefines.h>
 
 /* the viewpoint is from R, i.e. "get" means "Java->R" whereas "put" means "R->Java" */
@@ -11,6 +12,9 @@
 extern "C" {
 #endif
     
+#define SEXP2L(s) ((long)(s))
+#define L2SEXP(s) ((SEXP)((int)(s)))
+        
 jstring jri_callToString(JNIEnv *env, jobject o);
 
 SEXP jri_getDoubleArray(JNIEnv *env, jarray o);
@@ -18,11 +22,15 @@ SEXP jri_getIntArray(JNIEnv *env, jarray o);
 SEXP jri_getObjectArray(JNIEnv *env, jarray o);
 SEXP jri_getString(JNIEnv *env, jstring s);
 SEXP jri_getStringArray(JNIEnv *env, jarray o);
+SEXP jri_getSEXPLArray(JNIEnv *env, jarray o);
+
+SEXP jri_installString(JNIEnv *env, jstring s); /* as Rf_install, just for Java strings */
 
 jarray  jri_putDoubleArray(JNIEnv *env, SEXP e);
 jarray  jri_putIntArray(JNIEnv *env, SEXP e);
 jstring jri_putString(JNIEnv *env, SEXP e, int ix); /* ix=index, 0=1st */
 jarray  jri_putStringArray(JNIEnv *env, SEXP e);
+jarray jri_putSEXPLArray(JNIEnv *env, SEXP e); /* SEXPs are strored as "long"s */
 
 #ifdef __cplusplus
 }
