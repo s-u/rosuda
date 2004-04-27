81f71bf85492177f30fcea9dc6ab28ba3ddc9c91
Simon Urbanek <simon.urbanek@r-project.org>
2004-04-27 16:49:44 +0000

    Fixed bug in lockWithTimeout (wrong return value); Added verbose static flag for debugging purposes
    git-svn-id: svn://svn.rforge.net/org/trunk@806 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/JRI/Mutex.java
+++ rosuda/JRI/Mutex.java
@@ -2,6 +2,8 @@ package org.rosuda.JRI;
 
 /** This class implements a (not so) simple mutex. The initial state of the mutex is unlocked. */
 public class Mutex {
+    public static boolean verbose=false;
+    
     /** defines the current mutex state */
     private boolean locked=false;
     
@@ -14,13 +16,18 @@ public class Mutex {
         while (locked) {
             if (lockedBy==Thread.currentThread())
                 System.err.println("FATAL ERROR: org.rosuda.JRI.Mutex detected a deadlock! The application is likely to hang indefinitely!");
+            if (verbose)
+                System.out.println("INFO: "+toString()+" is locked by "+lockedBy+", but "+Thread.currentThread()+" waits for release (no timeout)");
             try {
                 wait();
             } catch (InterruptedException e) {
+                if (verbose)
+                    System.out.println("INFO: "+toString()+" caught InterruptedException");
             }
         }
         locked=true;
         lockedBy=Thread.currentThread();
+        if (verbose) System.out.println("INFO: "+toString()+" locked by "+lockedBy);
     }
     
     /** locks the mutex. If the mutex is already locked, waits until the mutex becomes free. Make sure the same thread doesn't issue two locks, because that will cause a deadlock.
@@ -32,33 +39,45 @@ public class Mutex {
         if (locked) {
             if (lockedBy==Thread.currentThread())
                 System.err.println("FATAL ERROR: org.rosuda.JRI.Mutex detected a deadlock! The application is likely to hang indefinitely!");
+            if (verbose)
+                System.out.println("INFO: "+toString()+" is locked by "+lockedBy+", but "+Thread.currentThread()+" waits for release (timeout "+to+" ms)");
             try {
                 wait(to);
             } catch (InterruptedException e) {
+                if (verbose)
+                    System.out.println("INFO: "+toString()+" caught InterruptedException");
             }
         }
         if (!locked) {
             locked=true;
             lockedBy=Thread.currentThread();
+            if (verbose) System.out.println("INFO: "+toString()+" locked by "+lockedBy);
+            return true;
         }
-        return locked;
+        if (verbose) System.out.println("INFO: "+toString()+" timeout, failed to obtain lock for "+Thread.currentThread());
+        return false;
     }
     
     /** attempts to lock the mutex and returns information about its success.
         @return 0 if the mutex was locked sucessfully<br>1 if the mutex is already locked by another thread<br>-1 is the mutex is already locked by the same thread (hence a call to {@link lock()} would cause a deadlock). */
     public synchronized int tryLock()
     {
-            if (locked) return (lockedBy==Thread.currentThread())?-1:1;
-            locked=true;
-            lockedBy=Thread.currentThread();
-            return 0;
+        if (verbose) System.out.println("INFO: "+toString()+" tryLock by "+Thread.currentThread());
+        if (locked) return (lockedBy==Thread.currentThread())?-1:1;
+        locked=true;
+        lockedBy=Thread.currentThread();
+        if (verbose) System.out.println("INFO: "+toString()+" locked by "+lockedBy);
+        return 0;
     }
     
     /** Locks the mutex. It works like {@link lock()} except that it returns immediately if the same thread already owns the lock. It is safer to use this function rather than {@link lock()}, because lock can possibly cause a deadlock which won't be resolved.
         @return <code>true</code> is the mutex was successfully locked, <code>false</code> if deadlock was detected (i.e. the same thread has already the lock). */
     public synchronized boolean safeLock()
     {
-        if (locked && lockedBy==Thread.currentThread()) return false;
+        if (locked && lockedBy==Thread.currentThread()) {
+            if (verbose) System.out.println("INFO: "+toString()+" unable to provide safe lock for "+Thread.currentThread());
+            return false;
+        }
         lock();
         return true;
     }
@@ -67,7 +86,10 @@ public class Mutex {
         @return <code>true</code> is the mutex was successfully locked, <code>false</code> if deadlock was detected or timeout elapsed. */
     public synchronized boolean safeLockWithTimeout(long to)
     {
-        if (locked && lockedBy==Thread.currentThread()) return false;
+        if (locked && lockedBy==Thread.currentThread()) {
+            if (verbose) System.out.println("INFO: "+toString()+" unable to provide safe lock (deadlock detected) for "+Thread.currentThread());
+            return false;
+        }
         return lockWithTimeout(to);
     }
     
@@ -77,6 +99,8 @@ public class Mutex {
         if (locked && lockedBy!=Thread.currentThread())
             System.err.println("WARNING: org.rosuda.JRI.Mutex was unlocked by other thread than locked! This may soon lead to a crash...");
         locked=false;
+        if (verbose) System.out.println("INFO: "+toString()+" unlocked by "+Thread.currentThread());
+
         // notify just 1 in case more of them are waiting
         notify();
     }
