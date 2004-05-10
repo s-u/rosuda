f8a8a3cd6a4d2156dc502eda9b9c5205f85cc80b
Simon Urbanek <simon.urbanek@r-project.org>
2004-05-10 18:03:53 +0000

    Prepare for menu inclusion (but we'll need some more changed esp. with 'close' before we can enable it)
    git-svn-id: svn://svn.rforge.net/org/trunk@858 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/JGR/toolkit/JavaGD.java
+++ rosuda/JGR/toolkit/JavaGD.java
@@ -9,9 +9,11 @@
 package org.rosuda.JGR.toolkit;
 
 import org.rosuda.JRI.Rengine;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 /** Implementation of JavaGD which uses iFrame instead of Frame */
-public class JavaGD extends org.rosuda.javaGD.JavaGD {
+public class JavaGD extends org.rosuda.javaGD.JavaGD implements ActionListener {
     iFrame ifr;
 
     public void     gdOpen(double w, double h) {
@@ -19,6 +21,13 @@ public class JavaGD extends org.rosuda.javaGD.JavaGD {
 
         ifr=new iFrame("JavaGD", iFrame.clsJavaGD);
         ifr.addWindowListener(this);
+
+        /* -- too much trouble with this - especially the "close" part...
+         String[] Menu = { "~Window", "0" };
+         iMenu.getMenu(ifr, this, Menu);
+         */
+        
+        ifr.setDefaultCloseOperation(ifr.DISPOSE_ON_CLOSE);
         c=new org.rosuda.javaGD.GDCanvas(w, h);
         ifr.setSize((int)w,(int)h); //added because sometimes the device isn't shown
         ifr.getContentPane().add(c);
@@ -33,5 +42,10 @@ public class JavaGD extends org.rosuda.javaGD.JavaGD {
             ifr.dispose();
             ifr=null;
         }
+    }
+
+    // we'll use this once the menu is available ...
+    public void actionPerformed(ActionEvent e) {
+        String cmd = e.getActionCommand();
     }
 }
