155d121822680aa870dfd0f4a654a0feada0dd58
Markus Helbig <info@markushelbig.de>
2004-05-18 13:24:35 +0000

    No log message.
    git-svn-id: svn://svn.rforge.net/org/trunk@878 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/JRI/Rengine.java
+++ rosuda/JRI/Rengine.java
@@ -7,10 +7,7 @@ public class Rengine extends Thread {
     static {
         try {
             System.loadLibrary("jri");
-        } catch (RuntimeException e) {
-            System.out.println("jri not found");
-            System.exit(1);
-        } catch (Exception e) {
+        } catch (UnsatisfiedLinkError e) {
             System.out.println("jri not found");
             System.exit(1);
         }
