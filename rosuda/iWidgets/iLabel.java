1895dd4a392ea092d5eef32b8727fd25722b4bf5
Simon Urbanek <simon.urbanek@r-project.org>
2004-07-21 19:25:49 +0000

    A simple label/text (JLabel).
    git-svn-id: svn://svn.rforge.net/org/trunk@979 955e8658-2bef-402f-a597-8b5c18d8b76d

---
+++ rosuda/iWidgets/iLabel.java
@@ -0,0 +1,18 @@
+//
+//  iLabel.java
+//  iWidgets
+//
+//  Created by Simon Urbanek on Wed Jul 21 2004.
+//  Copyright (c) 2004 Simon Urbanek. All rights reserved.
+//
+//  $Id$
+
+package org.rosuda.iWidgets;
+
+import javax.swing.*;
+
+public class iLabel extends JLabel {
+    public iLabel(String text) {
+        super(text);
+    }
+}
