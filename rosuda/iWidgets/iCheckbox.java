98d4913d8301353c1d6089c99dbe9198f0db37d3
Simon Urbanek <simon.urbanek@r-project.org>
2004-07-22 14:45:22 +0000

    Simple checkbox implementation
    git-svn-id: svn://svn.rforge.net/org/trunk@984 955e8658-2bef-402f-a597-8b5c18d8b76d

---
+++ rosuda/iWidgets/iCheckbox.java
@@ -0,0 +1,18 @@
+//
+//  iCheckbox.java
+//  JRGui
+//
+//  Created by Simon Urbanek on Thu Jul 22 2004.
+//  Copyright (c) 2004 Simon Urbanek. All rights reserved.
+//
+//  $Id$
+
+package org.rosuda.iWidgets;
+
+import javax.swing.*;
+
+public class iCheckbox extends JCheckBox {
+    iCheckbox(String text, boolean checked) {
+        super(text, checked);
+    }
+}
