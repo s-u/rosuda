9b0c8d1e62f59f35e36053380727ab757901ad5e
Simon Urbanek <simon.urbanek@r-project.org>
2004-07-22 15:12:31 +0000

    Simple text edit field (JTextField).
    git-svn-id: svn://svn.rforge.net/org/trunk@986 955e8658-2bef-402f-a597-8b5c18d8b76d

---
+++ rosuda/iWidgets/iEdit.java
@@ -0,0 +1,20 @@
+//
+//  iEdit.java
+//  iWidgets
+//
+//  Created by Simon Urbanek on Thu Jul 22 2004.
+//  Copyright (c) 2004 Simon Urbanek. All rights reserved.
+//
+//  $Id$
+
+package org.rosuda.iWidgets;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class iEdit extends JTextField {
+    public iEdit(String text, int cols) {
+        super(text, cols);
+    }
+}
