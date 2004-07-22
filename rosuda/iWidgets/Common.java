a3baf26647e0d45be2e8c92c4f2d936b63a9955e
Simon Urbanek <simon.urbanek@r-project.org>
2004-07-22 20:20:45 +0000

    Common iWidget globals
    git-svn-id: svn://svn.rforge.net/org/trunk@987 955e8658-2bef-402f-a597-8b5c18d8b76d

---
+++ rosuda/iWidgets/Common.java
@@ -0,0 +1,15 @@
+//
+//  Common.java
+//  JRGui
+//
+//  Created by Simon Urbanek on Thu Jul 22 2004.
+//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+//
+
+package org.rosuda.iWidgets;
+
+public class Common {
+    public static final int iVersion=0x0100;
+
+    public static int getIntVersion() { return iVersion; }
+}
