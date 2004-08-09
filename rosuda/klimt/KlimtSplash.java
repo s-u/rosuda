0888797abd1f06a0643f428ab4f352a8b5766cfa
Simon Urbanek <simon.urbanek@r-project.org>
2004-08-09 20:01:22 +0000

    Added support for task-files (loading of data and trees). Few cleanups - the recent list is updated only in KlimtSplash to filter out non-data loads.
    git-svn-id: svn://svn.rforge.net/org/trunk@1056 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/klimt/KlimtSplash.java
+++ rosuda/klimt/KlimtSplash.java
@@ -11,6 +11,8 @@ package org.rosuda.klimt;
 import org.rosuda.ibase.*;
 import org.rosuda.ibase.toolkit.*;
 import org.rosuda.util.*;
+import java.io.*;
+import java.awt.FileDialog;
 
 public class KlimtSplash extends SplashScreen {
     public KlimtSplash() {
@@ -39,12 +41,81 @@ public class KlimtSplash extends SplashScreen {
         if (cmd=="openData") {
             SVarSet tvs=new SVarSet();
             DataRoot dr=Klimt.addData(tvs);
+            
+            if (openFn==null) {
+                FileDialog fd=new FileDialog(this,"Select data file");
+		fd.setModal(true);
+		fd.show();
+		openFn=fd.getDirectory()+fd.getFile();
+		if (fd.getFile()==null) return null;
+            }
+            
+            File f=new File(openFn);
+            
+            try {
+                String prefix=f.getParent();
+                BufferedReader r=new BufferedReader(new InputStreamReader(new FileInputStream(f)));
+                String s=r.readLine();
+                if (s.startsWith("### KLIMT.TaskFile 1")) {
+                    // ok, the file seems to work, so let's put it in the recent list
+                    if (SplashScreen.recentOpen!=null) SplashScreen.recentOpen.addEntry(f.getAbsolutePath());
+                    if (Global.DEBUG>0) System.out.println("KlimtSplash.run:openData: encountered KLIMT task file ("+s+"), processing");
+                    boolean displayNew=true;
+                    boolean treeLoadBatch=false;
+                    while (r.ready()) {
+                        s=r.readLine();
+                        if (s.equals(">end.load.trees")) treeLoadBatch=false;
+                        if (s.startsWith(">load.data ") || s.startsWith(">load.tree ") || treeLoadBatch) {
+                            boolean treeOnly=treeLoadBatch || s.startsWith(">load.tree ");
+                            String fn=(treeLoadBatch)?s:s.substring(11);
+                            if (fn.length()>0) {
+                                if (fn.charAt(0)!=File.separatorChar)
+                                    fn=prefix+File.separator+fn;
+                                if (Global.DEBUG>0) System.out.println("KlimtSplash.run:openData: load.xxx of \""+fn+"\"");
+                                if (!treeOnly && tvs.count()>0) { // create new data if there's already a datafile
+                                    tvs=new SVarSet();
+                                    dr=Klimt.addData(tvs);
+                                }
+                                File dfn=new File(fn);
+                                SNode t=Klimt.loadTreeFile(dfn, dr, displayNew);
+                                if (!treeOnly && tvs.count()>0) {
+                                    if (tvs.getMarker()==null && (tvs.at(0)!=null)&&(tvs.at(0).size()>0))
+                                        tvs.setMarker(new SMarker(tvs.at(0).size()));
+                                    VarFrame vf=Klimt.newVarDisplay(dr,Common.screenRes.width-150,0,140,(Common.screenRes.height>600)?600:Common.screenRes.height-30);
+                                    setVisible(false);
+                                }
+                            }
+                        }
+                        if (s.startsWith(">display.new ")) {
+                            String yn=s.substring(13);
+                            if (yn.startsWith("y")) displayNew=true;
+                            if (yn.startsWith("n")) displayNew=false;
+                        }
+                        if (s.equals(">end")) break;
+                        if (s.equals(">begin.load.trees")) treeLoadBatch=true;
+                    }
+                    String wars=Common.getWarnings();
+                    if (wars!=null) {
+                        HelpFrame hf=new HelpFrame();
+                        hf.t.setText("Following warnings were produced during task processing:\n\n"+wars);
+                        hf.setTitle("Load warnings");
+                        //hf.setModal(true);
+                        hf.show();
+                    }
+                    return null;
+                }
+            } catch (Exception tfe) {
+                System.out.println("Task file detection failed: "+tfe.getMessage());
+                tfe.printStackTrace();
+            }
+            
             SNode t=Klimt.openTreeFile(this,openFn,dr);
             if (t==null && tvs.count()<1) {
             } else {
+                if (SplashScreen.recentOpen!=null) SplashScreen.recentOpen.addEntry(f.getAbsolutePath());
                 if (t!=null) {
-                    TFrame f=new TFrame("Tree "+tvs.getName(),TFrame.clsTree);
-                    Klimt.newTreeDisplay(t,f,0,0,Common.screenRes.width-160,(Common.screenRes.height>600)?600:Common.screenRes.height-20);
+                    TFrame fr=new TFrame("Tree "+tvs.getName(),TFrame.clsTree);
+                    Klimt.newTreeDisplay(t,fr,0,0,Common.screenRes.width-160,(Common.screenRes.height>600)?600:Common.screenRes.height-20);
                 }
                 VarFrame vf=Klimt.newVarDisplay(dr,Common.screenRes.width-150,0,140,(Common.screenRes.height>600)?600:Common.screenRes.height-30);
                 setVisible(false);
