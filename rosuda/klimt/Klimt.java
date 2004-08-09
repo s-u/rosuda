36937086f7dbcf085afa7b427847a8371386268b
Simon Urbanek <simon.urbanek@r-project.org>
2004-08-09 21:47:55 +0000

    Bumped up the pre-version.
    git-svn-id: svn://svn.rforge.net/org/trunk@1063 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/klimt/Klimt.java
+++ rosuda/klimt/Klimt.java
@@ -33,8 +33,8 @@ import org.rosuda.plugins.*;
     original project name until it was renamed to Klimt) */
 public class Klimt
 {
-    public static String Version = "0.98-pre3";
-    public static String Release = "E703";
+    public static String Version = "0.98-pre5";
+    public static String Release = "E809";
     
     /** file name of the most recently loaded tree. Because of more recent support of multiple trees the use of the variable is deprecated for external packages. */
     public static String lastTreeFileName;
