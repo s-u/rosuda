d3827cbf8eb312e14e7673f702632a7b6baff9eb
Simon Urbanek <simon.urbanek@r-project.org>
2004-09-28 18:21:50 +0000

    Added support for --debug flag.
    git-svn-id: svn://svn.rforge.net/org/trunk@1188 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/JGR/JGR.java
+++ rosuda/JGR/JGR.java
@@ -241,6 +241,11 @@ public class JGR {
     }
 
     public static void main(String[] args) {
+		if (args.length>0 && args[0].equals("--debug")) {
+			org.rosuda.util.Global.DEBUG=1;
+			org.rosuda.JRI.DEBUG=1;
+			System.out.println("JGR version " + VERSION);
+		}
         try {
             new JGR();
         }
