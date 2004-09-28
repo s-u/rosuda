32bf0b461c78c1a7d529463d2513247b0358d60e
Simon Urbanek <simon.urbanek@r-project.org>
2004-09-28 18:22:54 +0000

    Fixed bug in --debug handling.
    git-svn-id: svn://svn.rforge.net/org/trunk@1190 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/JGR/JGR.java
+++ rosuda/JGR/JGR.java
@@ -243,7 +243,7 @@ public class JGR {
     public static void main(String[] args) {
 		if (args.length>0 && args[0].equals("--debug")) {
 			org.rosuda.util.Global.DEBUG=1;
-			org.rosuda.JRI.DEBUG=1;
+			org.rosuda.JRI.Rengine.DEBUG=1;
 			System.out.println("JGR version " + VERSION);
 		}
         try {
