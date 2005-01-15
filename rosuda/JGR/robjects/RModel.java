353583ba9afaf2ba7a9a2f27d6a17266d58ed855
Markus Helbig <info@markushelbig.de>
2005-01-15 13:01:53 +0000

    fully documented
    git-svn-id: svn://svn.rforge.net/org/trunk@1308 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/JGR/robjects/RModel.java
+++ rosuda/JGR/robjects/RModel.java
@@ -1,13 +1,11 @@
 package org.rosuda.JGR.robjects;
 
 /**
- *  RModel
- * 
- * 	java-representation of a model in R
+ *  RModel - This is a simple java-representation of a model in R (currently only lm and glm are implemented) providing several information.
  * 
  *	@author Markus Helbig
- *  
- * 	RoSuDA 2003 - 2004 
+ * 	
+ *  RoSuDA 2003 - 2005 
  */
 
 import java.text.*;
@@ -33,64 +31,119 @@ public class RModel {
     
     private String name;
 
-   
     public RModel(String name, String type) {
         this.name = name;
         if (type != null) this.type = type;
     }
 
+    /**
+     * Get the type of the model, like lm, glm.
+     * @return r-class of model
+     */
     public String getTypeName() {
     	return type;
     }
     
+    /**
+     * Get the name of the model.
+     * @return  name of model
+     */
     public String getName() {
     	return name;
     }
 
+    /**
+     * Set the call statement from R concerning the model.
+     * @param call call-statement
+     */
     public void setCall(String call) {
         this.call = call;
     }
 
+    /**
+     * Get call-statement of the model.
+     * @return call-statement
+     */
     public String getCall() {
         return call;
     }
     
+    /**
+     * Get tooltip-text implementation, shows the call statement.
+     * @return tooltip-text
+     */
     public String getToolTip() {
     	return "<html><pre>"+call+"</pre></html>";
     }
 
+    /**
+     * Set r-square value of the model.
+     * @param r r-square value
+     */
     public void setRsquared(double r) {
         this.rsquared = new Double(dformat.format(r).replace(',','.'));
     }
     
+    /**
+     * Set deviance value of model.
+     * @param d deviance value
+     */
     public void setDeviance(double d) {
         this.deviance = new Double(dformat.format(d).replace(',','.'));
     }
     
+    /**
+     * Set degree of freedom value of model.
+     * @param df degree of freedom value
+     */
     public void setDf(int df) {
         this.df = new Integer(df);
     }
     
+    /**
+     * Set AIC value of model.
+     * @param a AIC value
+     */
     public void setAic(double a) {
         this.aic = new Double(dformat.format(a).replace(',','.'));
     }
     
+    /**
+     * Set F-statistic value of model.
+     * @param f F-statistic value
+     */
     public void setFstat(double f) {
         this.fstatistics = new Double(dformat.format(f).replace(',','.'));
     }
 
+    /**
+     * Set family name of model, needed with glm-models: binomial, poisson ....
+     * @param f family name
+     */
     public void setFamily(String f) {
         this.family = f;
     }
 
+    /**
+     * Set data name of model, which means the data the model is based on.
+     * @param d data name
+     */
     public void setData(String d) {
         this.data = d;
     }
 
+    /**
+     * Get data name of model.
+     * @return data-name
+     */
     public String getData() {
         return data;
     }
 
+    /**
+     * Get information about this model: name, data, type, family, degree-of-freedom, r-square, aic, deviance.
+     * @return information
+     */
     public Vector getInfo() {
         if (info.size() == 0) {
             info.add(getName());
--- rosuda/JGR/robjects/RObject.java
+++ rosuda/JGR/robjects/RObject.java
@@ -1,13 +1,11 @@
 package org.rosuda.JGR.robjects;
 
 /**
- *  RObject
- * 
- * 	java-represesantion of an object out of R
+ *  RModel - This is a simple java-representation of an object from R providing some information about it.
  * 
  *	@author Markus Helbig
  *  
- * 	RoSuDA 2003 - 2004 
+ * 	RoSuDA 2003 - 2005 
  */
 
 public class RObject {
@@ -21,6 +19,13 @@ public class RObject {
     
     private boolean realName = false;
 
+    /**
+     * Create a new RObject
+     * @param name name of object
+     * @param type type of object
+     * @param parent parent object, e.g. if it's an inner vector of a data.frame
+     * @param b is there are real R-name or should we set a number instead
+     */
     public RObject(String name, String type, RObject parent, boolean b) {
         this.name = name;
         this.type = type;
@@ -28,42 +33,80 @@ public class RObject {
         this.realName = b;
     }
 
+    /**
+     * Create a new RObject
+     * @param name name of object
+     * @param type type of object
+     * @param b is there are real R-name or should we set a number instead
+     */
     public RObject(String name, String type, boolean b) {
     	this(name,type,null, b);
     }
     
+    /**
+     * Set name of object.
+     * @param name name of object
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Get name of object
+     * @return name of object
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Get type of object: numeric, character ....
+     * @return type of object
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * Set parent object.
+     * @param p parent object
+     */
     public void setParent(RObject p) {
         this.parent = p;
     }
     
+    /**
+     * Get parent object.
+     * @return parent object
+     */
     public RObject getParent() {
     	return parent;
     }
 
+    /**
+     * Check wether the object is atomar or not, atomar objects are: function, integer, numeric, character, logical, factor, environment (because they are not supported).
+     * @return true if it's atomar, false if it is not
+     */
     public boolean isAtomar() {
         if (type.indexOf("function") >= 0 || type.indexOf("integer") >= 0 || type.indexOf("numeric") >= 0 || type.indexOf("character") >= 0 || type.indexOf("logical") >= 0 || type.indexOf("factor") >= 0 || type.indexOf("environment") >= 0)
             return true;
         return false;
     }
     
+    /**
+     * Check if editing this object is supported, types which are supported are: numeric, integer, factor, character, data.frame, matrix, list.
+     * @return true if editing is allowed, false if not
+     */
     public boolean isEditable() {
         if (type.indexOf("numeric") >= 0 || type.indexOf("integer") >= 0 || type.indexOf("factor") >= 0 || type.indexOf("character") >= 0 || type.indexOf("data.frame") >= 0 || type.indexOf("matrix") >= 0 || type.indexOf("list") >= 0)
             return true;
         return false;
     }
 
+    /**
+     * Get real R-name of this object, e.g.: Cars[["displacement"]].
+     * @return R-name
+     */
     public String getRName() {
     	if (parent != null && parent.getType().equals("matrix"))
     		return parent.getName()+"[,"+(realName?"\"":"")+getName()+(realName?"\"":"")+"]";
@@ -72,10 +115,18 @@ public class RObject {
     	return parent==null?getName():parent.getRName()+"[["+(realName?"\"":"")+getName()+(realName?"\"":"")+"]]";
     }
     
+    /**
+     * Set information about object, depends on object: data.frame -> dimension, function -> arguments, factor -> levels, .... 
+     * @param s information depending on object
+     */
     public void setInfo(String s){
     	this.info = s;
     }
 
+    /**
+     * Get provided information about this object.
+     * @return information
+     */
     public String getInfo() {
     	return info;
     }
