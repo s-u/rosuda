698e71acc32588420e5e8814f29aa6aeaca7e4ab
Martin Theus <martin@theusrus.de>
2005-01-24 19:12:57 +0000

    white spaces
    git-svn-id: svn://svn.rforge.net/org/trunk@1335 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/Mondrian/Mondrian.java
+++ rosuda/Mondrian/Mondrian.java
@@ -1,15 +1,18 @@
 import java.util.Vector;         //
 
-public class Mondrian
+public class Mondrian 
 {
 
   protected Vector dataSets = new Vector(5,5);
 
   /** A very simple main() method for our program. */
-  public static void main(String[] args) { new Mondrian(); }
+  public static void main(String[] args) {
+    new Mondrian();
+  }
 
   public Mondrian() {
-    new Join(dataSets, false, false);
+    new Join(dataSets, false, false, null);
   }
+
 }
 
