c7870d8ad5e92731fdea9d0164f2eb2e8225b3f8
Markus Helbig <info@markushelbig.de>
2005-01-26 21:55:10 +0000

    changed: 1.1a-pre
    git-svn-id: svn://svn.rforge.net/org/trunk@1348 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/JGR/JGR.java
+++ rosuda/JGR/JGR.java
@@ -33,7 +33,7 @@ public class JGR {
 	/* Copyright information and other stuff */
 
 	/** Version number of JGR */
-	public static final String VERSION = "1.1";
+	public static final String VERSION = "1.1a-pre";
 
 	/** Title (used for displaying the splashscreen) */
 	public static final String TITLE = "JGR";
