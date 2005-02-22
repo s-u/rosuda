c4a2951dc5b1513fc56aac5d9341a57269302231
Simon Urbanek <simon.urbanek@r-project.org>
2005-02-22 23:09:42 +0000

    MapScatterCanvas - scatter plot with a map underneath
    git-svn-id: svn://svn.rforge.net/org/trunk@1397 955e8658-2bef-402f-a597-8b5c18d8b76d

---
+++ rosuda/ibase/plots/MapScatterCanvas.java
@@ -0,0 +1,40 @@
+//
+//  MapScatterPlot.java
+//  Klimt
+//
+//  Created by Simon Urbanek on 22.02.05.
+//  Copyright 2005 Simon Urbanek. All rights reserved.
+//
+
+package org.rosuda.ibase.plots;
+
+import java.awt.*;
+import org.rosuda.pograss.*;
+import org.rosuda.ibase.*;
+import org.rosuda.ibase.toolkit.*;
+
+/** Scatter plot overlaying a map */
+public class MapScatterCanvas extends ScatterCanvas {
+	// map variable
+	SVar map;
+    public MapScatterCanvas(Frame f, SVar v1, SVar v2, SVar map, SMarker mark) {
+		super(f, v1, v2, mark);
+		this.map = map;
+	}
+	
+	public void paintBackground(PoGraSS g) {
+		int i=0;
+		g.setColor("black");
+		while (i<map.size()) {
+			MapSegment ms=(MapSegment) map.at(i);
+            if (ms!=null) {
+                int j=0;
+                while (j<ms.count()) {
+					g.drawPolygon(MapSegmentTools.transViaAxisX(ms,j,A[0]),MapSegmentTools.transViaAxisY(ms,j,A[1]),ms.getSizeAt(j));
+                    j++;
+                }
+            }
+			i++;
+		}			
+	}
+}
