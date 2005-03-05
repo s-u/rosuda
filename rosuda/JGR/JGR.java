0935d1b3c71180eb8b5744be49a1b47a2e39a4fc
Markus Helbig <info@markushelbig.de>
2005-03-05 09:44:06 +0000

    fixed: version number
    git-svn-id: svn://svn.rforge.net/org/trunk@1406 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/JGR/JGR.java
+++ rosuda/JGR/JGR.java
@@ -37,7 +37,7 @@ public class JGR {
 	/* Copyright information and other stuff */
 
 	/** Version number of JGR */
-	public static final String VERSION = "1.1a";
+	public static final String VERSION = "1.1a (nightly)";
 
 	/** Title (used for displaying the splashscreen) */
 	public static final String TITLE = "JGR";
@@ -54,7 +54,7 @@ public class JGR {
 	/** Author JGR (used for displaying the splashscreen) */
 	public static final String AUTHOR1 = "Markus Helbig";
 
-	/** Author JRI and JavaGD (used for displaying the splashscreen) */
+	/** Author JRI, rJava and JavaGD (used for displaying the splashscreen) */
 	public static final String AUTHOR2 = "Simon Urbanek";
 
 	/** Website of organization (used for displaying the splashscreen) */
