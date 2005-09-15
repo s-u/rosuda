e58ed3fc614b9bc0c030b6fdb43f5fb2fc892f89
Tobias Wichtrey <wichtrey@math.uni-augsburg.de>
2005-09-15 12:16:59 +0000

    add obs, censored, dir
    git-svn-id: svn://svn.rforge.net/org/trunk@1576 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/ibase/toolkit/PPrimMosaic.java
+++ rosuda/ibase/toolkit/PPrimMosaic.java
@@ -5,30 +5,56 @@ import org.rosuda.pograss.PoGraSS;
 
 
 public class PPrimMosaic extends PPrimRectangle {
-	
-	public String info = null;
-	
-	public boolean empty = false;
-	
-	//get Information about this mosaic
-	public String toString() {
-		return info;
-	}
-	
-	//if empty paint in red
+    
+    public String info = null;
+    public boolean empty = false;
+    private double obs;
+    private char dir;
+    private boolean censored;
+    
+    //get Information about this mosaic
+    public String toString() {
+        return info;
+    }
+    
+    //if empty paint in red
     public void paint(PoGraSS g, int orientation) {
-    	if (r==null) return;
+        if (r==null) return;
         if (col!=null)
             g.setColor(col.getRed(),col.getGreen(),col.getBlue());
         else
             g.setColor("object");
-    	if (empty)
-    		g.setColor(Color.red);
-    	if (!empty) 
-    		g.fillRect(r.x,r.y,r.width,r.height);
+        if (empty)
+            g.setColor(Color.red);
+        if (!empty)
+            g.fillRect(r.x,r.y,r.width,r.height);
         if (drawBorder) {
             if (!empty) g.setColor("outline");
             g.drawRect(r.x,r.y,r.width,r.height);
         }
-    }	
+    }
+
+    public double getObs() {
+        return obs;
+    }
+
+    public void setObs(double obs) {
+        this.obs = obs;
+    }
+
+    public char getDir() {
+        return dir;
+    }
+
+    public void setDir(char dir) {
+        this.dir = dir;
+    }
+
+    public boolean isCensored() {
+        return censored;
+    }
+
+    public void setCensored(boolean censored) {
+        this.censored = censored;
+    }
 }
