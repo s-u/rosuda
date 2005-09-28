be55c6fd8b99ec528508fe8921c9da163bf204ef
Tobias Wichtrey <wichtrey@math.uni-augsburg.de>
2005-09-28 12:42:05 +0000

    add p, exp, scale
    git-svn-id: svn://svn.rforge.net/org/trunk@1620 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/ibase/toolkit/PPrimMosaic.java
+++ rosuda/ibase/toolkit/PPrimMosaic.java
@@ -20,6 +20,10 @@ public class PPrimMosaic extends PPrimRectangle {
     private int type=TYPE_OBSERVED;
     private int origX,origY,fullW,fullH;
     
+    private double p;
+    private double exp;
+    private double scale;
+    
     //get Information about this mosaic
     public String toString() {
         return info;
@@ -59,6 +63,48 @@ public class PPrimMosaic extends PPrimRectangle {
                     g.drawRect(r.x,r.y,r.width,r.height);
                 }
         }
+        if( type==TYPE_EXPECTED ) {
+            int high = (int)(192+63*(0.15+pnorm((1-p-0.9)*10)));
+            int low =  (int)(192*(0.85-pnorm((1-p-0.9)*10)));
+            if( obs-exp > 0.00001 )
+                g.setColor(new Color(low, low, high));
+            else if( obs-exp < -0.00001 )
+                g.setColor(new Color(high, low, low));
+            else
+                g.setColor(Color.lightGray);
+            if( dir == 'x' )
+                g.fillRect(r.x, r.y, (int)((double)r.width*Math.abs((obs-exp)/Math.sqrt(exp)*scale)), r.height);
+            else if ( dir == 'y' )
+                g.fillRect(r.x, r.y+r.height-(int)((double)r.height*Math.abs((obs-exp)/Math.sqrt(exp)*scale)),
+                        r.width, (int)((double)r.height*Math.abs((obs-exp)/Math.sqrt(exp)*scale)));
+        }
+    }
+    
+    private double pnorm( double q ) {
+        
+        double up=0.9999999;
+        double lp=0.0000001;
+        while( Math.abs( up - lp ) > 0.0001 )
+            if( qnorm( (up+lp)/2 ) <= q )
+                lp = (up+lp)/2;
+            else
+                up = (up+lp)/2;
+        return up;
+    }
+    
+    private double qnorm( double p ) {
+        
+        double a0 = 2.515517;
+        double a1 = 0.802853;
+        double a2 = 0.010328;
+        
+        double b1 = 1.432788;
+        double b2 = 0.189269;
+        double b3 = 0.001308;
+        
+        double  t = Math.pow(-2*Math.log(1-p), 0.5);
+        
+        return t - (a0 + a1*t + a2*t*t) / (1 + b1*t + b2*t*t + b3*t*t*t);
     }
     
     public void setObs(double obs){
@@ -101,5 +147,17 @@ public class PPrimMosaic extends PPrimRectangle {
         r.y += fullH - newHeight;
         r.width = newWidth;
         r.height = newHeight;
+    }
+
+    public void setP(double p) {
+        this.p = p;
+    }
+
+    public void setExp(double exp) {
+        this.exp = exp;
+    }
+
+    public void setScale(double scale) {
+        this.scale = scale;
     }
 }
