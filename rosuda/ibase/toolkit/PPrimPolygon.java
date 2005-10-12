1088192ffcf28af33331107315eb9e4561a50d25
Tobias Wichtrey <wichtrey@math.uni-augsburg.de>
2005-10-12 14:43:24 +0000

    add flags fill and closed
    git-svn-id: svn://svn.rforge.net/org/trunk@1645 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/ibase/toolkit/PPrimPolygon.java
+++ rosuda/ibase/toolkit/PPrimPolygon.java
@@ -17,49 +17,55 @@ import org.rosuda.pograss.*;
 import org.rosuda.util.*;
 
 /** Plot primitive based on {@link PPrimBase},  using a list of IDs and implementing polygons.
-@version $Id$
-*/
+ * @version $Id$
+ */
 public class PPrimPolygon extends PPrimBase {
     public Polygon pg;
     
     public boolean drawBorder=true;
     public boolean useSelAlpha=true;
-
+    public boolean closed=true;
+    public boolean fill=true;
+    
     /** checks whether the PlotPrimitive contains the given point.*/
     public boolean contains(int x, int y) { return (pg==null)?false:pg.contains(x,y); }
-
+    
     /** checks whether the PlotPrimitive intersects (or is contained) in the given rectangle. */
     public boolean intersects(Rectangle rt) { return (pg==null)?false:pg.intersects(rt); }
-
+    
     /** paint the primitive */
     public void paint(PoGraSS g, int orientation) {
         if (pg==null) return;
-        if (col!=null)
-            g.setColor(col.getRed(),col.getGreen(),col.getBlue());
-        else
-            g.setColor("object");
-        g.fillPolygon(pg.xpoints,pg.ypoints,pg.npoints);
+        if(fill){
+            if (col!=null)
+                g.setColor(col.getRed(),col.getGreen(),col.getBlue());
+            else
+                g.setColor("object");
+            g.fillPolygon(pg.xpoints,pg.ypoints,pg.npoints);
+        }
         if (drawBorder) {
             g.setColor("outline");
-            g.drawPolygon(pg.xpoints,pg.ypoints,pg.npoints);
+            g.drawPolygon(pg.xpoints,pg.ypoints,pg.npoints,closed);
         }
     }
-
+    
     public void paintSelected(PoGraSS g, int orientation, SMarker m) {
         if (pg==null) return;
         double sa=getMarkedProportion(m,-1);
         //System.out.println("pp["+i+"] sa="+sa+" "+pp);
         if (sa>0d) {
-            if (useSelAlpha && sa<1.0)
-                g.setColor(((float)Common.selectColor.getRed())/255.0F,
-                           ((float)Common.selectColor.getGreen())/255.0F,
-                           ((float)Common.selectColor.getBlue())/255.0F,(float)sa);
-            else
-                g.setColor("marked");
-            g.fillPolygon(pg.xpoints,pg.ypoints,pg.npoints);
+            if(fill){
+                if (useSelAlpha && sa<1.0)
+                    g.setColor(((float)Common.selectColor.getRed())/255.0F,
+                            ((float)Common.selectColor.getGreen())/255.0F,
+                            ((float)Common.selectColor.getBlue())/255.0F,(float)sa);
+                else
+                    g.setColor("marked");
+                g.fillPolygon(pg.xpoints,pg.ypoints,pg.npoints);
+            }
             if (drawBorder) {
                 g.setColor("outline");
-                g.drawPolygon(pg.xpoints,pg.ypoints,pg.npoints);
+                g.drawPolygon(pg.xpoints,pg.ypoints,pg.npoints,closed);
             }
         }
     }
