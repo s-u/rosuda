edbca376ca4cb20b40d2d99318e8a90e61dd44e9
Alexander Gouberman <gouberman@math.uni-augsburg.de>
2005-11-04 09:47:28 +0000

    use query popups with STRG + MouseMove
    git-svn-id: svn://svn.rforge.net/org/trunk@1705 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/ibase/Common.java
+++ rosuda/ibase/Common.java
@@ -178,7 +178,8 @@ public class Common
     /** returns true if the supplied event corresponds to popup query trigger. */
     public static boolean isQueryTrigger(MouseEvent ev) {
         // Query = <ALT> + BUTTON1; since mac emulates B2 we don't impose this on a Mac
-        return Platform.isMac?(ev.isAltDown() && !ev.isControlDown()):((ev.getModifiers()&MouseEvent.BUTTON3_MASK)==MouseEvent.BUTTON3_MASK);
+//        return Platform.isMac?(ev.isAltDown() && !ev.isControlDown()):((ev.getModifiers()&MouseEvent.BUTTON3_MASK)==MouseEvent.BUTTON3_MASK);
+    	return ev.isControlDown();
     }
 
     /** returns true if the supplied event corresponds to object-move trigger */
