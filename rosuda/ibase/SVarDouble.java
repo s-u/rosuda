3507ea33cd3dc85d7958e636bcad0ddd3d2f7a64
Tobias Wichtrey <wichtrey@math.uni-augsburg.de>
2005-11-18 11:11:16 +0000

    use Collections.sort to compute ranks
    git-svn-id: svn://svn.rforge.net/org/trunk@1738 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/ibase/SVarDouble.java
+++ rosuda/ibase/SVarDouble.java
@@ -12,31 +12,29 @@ import java.util.*;
 import org.rosuda.util.*;
 
 /** SVar implementation for ed-length doubles variable
-*/
+ */
 
-public class SVarDouble extends SVar
-{
+public class SVarDouble extends SVar {
     /** the actual content */
     public double[] cont;
-
+    
     /** insertion point for add */
     int insertPos=0;
-
+    
     /** vector of categories if cat. var. */
     Vector  cats;
     /** vector if counts per category */
     Vector  ccnts;
-
+    
     int[] ranks=null;
-
+    
     static double[] temp;
-
+    
     /** construct new variable
-	@param Name variable name
-	@param len length of the ed variable
-        */
-    public SVarDouble(String Name, int len)
-    {
+     * @param Name variable name
+     * @param len length of the ed variable
+     */
+    public SVarDouble(String Name, int len) {
         super(Name, false);
         if (len<0) len=0;
         guessing=false;
@@ -46,13 +44,12 @@ public class SVarDouble extends SVar
         for (int i = 0; i < cont.length; i++) cont[i] = Double.NaN;
         insertPos = len;
     }
-
+    
     public SVarDouble(String Name, double[] d) {
         this(Name, d, true);
     }
-
-    public SVarDouble(String Name, double[] d, boolean copyContents)
-    {
+    
+    public SVarDouble(String Name, double[] d, boolean copyContents) {
         super(Name, false);
         boolean firstValid=true;
         min=max=0;
@@ -95,37 +92,37 @@ public class SVarDouble extends SVar
         contentsType=CT_Number;
         isnum=true;
     }
-
+    
     public int size() { return cont.length; }
-
+    
     /** define the variable explicitely as categorical
-	@param rebuild if set to <code>true</code> force rebuild even if the variable is already categorial. */
+     * @param rebuild if set to <code>true</code> force rebuild even if the variable is already categorial. */
     public void categorize(boolean rebuild) {
-	if (cat && !rebuild) return;
-	cats=new Vector(); ccnts=new Vector();
-	cat=true;
-	if (!isEmpty()) {
+        if (cat && !rebuild) return;
+        cats=new Vector(); ccnts=new Vector();
+        cat=true;
+        if (!isEmpty()) {
             int ci=0;
             while (ci<cont.length) {
                 String oo=Double.isNaN(cont[ci])?missingCat:Double.toString(cont[ci]);
-		int i=cats.indexOf(oo);
+                int i=cats.indexOf(oo);
                 if (i==-1) {
                     cats.addElement(oo);
                     ccnts.addElement(new Integer(1));
-		} else {
-		    ccnts.setElementAt(new Integer(((Integer)ccnts.elementAt(i)).intValue()+1),i);
-		}
+                } else {
+                    ccnts.setElementAt(new Integer(((Integer)ccnts.elementAt(i)).intValue()+1),i);
+                }
                 ci++;
-	    }
+            }
             if (isNum()) { // if numerical and categorical then sort categories for convenience
                 sortCategories(SM_num);
             }
-	}
+        }
         NotifyAll(new NotifyMsg(this,Common.NM_VarTypeChange));
     }
-
+    
     /** sort categories by specifeid method
-        @param method sort method, see SM_xxx constants */
+     * @param method sort method, see SM_xxx constants */
     public void sortCategories(int method) {
         if (!isCat() || cats.size()<2) return;
         Stopwatch sw=null;
@@ -177,13 +174,13 @@ public class SVarDouble extends SVar
             sw.profile("sorted");
         }
     }
-
+    
     /** define the variable explicitely as non-categorial (drop category list) */
     public void dropCat() {
-	cats=null; ccnts=null; cat=false;
+        cats=null; ccnts=null; cat=false;
         NotifyAll(new NotifyMsg(this,Common.NM_VarTypeChange));
     }
-
+    
     public void setCategorical(boolean nc) {
         if (!nc) {
             cat=false;
@@ -191,7 +188,7 @@ public class SVarDouble extends SVar
             if (cats==null) categorize(); else cat=true;
         }
     }
-
+    
     /** adds a new case to the variable (NEVER use addElement! see package header) Also beware, categorial varaibles are classified by object not by value!
      *  @param o object to be added. First call to <code>add</code> (even implicit if an object was specified on the call to the constructor) does also decide whether the variable will be numeric or not. If the first object is a subclass of <code>Number</code> then the variable is defined as numeric. There is a significant difference in handling numeric and non-numeric variabels, see package header.
      *  @return <code>true<code> if element was successfully added, or <code>false</code> upon failure - currently when non-numerical value is inserted in a numerical variable. It is strongly recommended to check the result and act upon it, because failing to do so can result in non-consistent datasets - i.e. mismatched row IDs */
@@ -208,41 +205,41 @@ public class SVarDouble extends SVar
         }
         return add(val);
     }
-
+    
     public boolean add(int i) { return add((i==int_NA)?double_NA:((double)i)); }
-
+    
     public boolean add(double d) {
         if (insertPos>=cont.length) return false;
-	if (cat) {
+        if (cat) {
             Object oo=Double.isNaN(d)?missingCat:Double.toString(d);
-	    int i=cats.indexOf(oo);
-	    if (i==-1) {
-		cats.addElement(oo);
-		ccnts.addElement(new Integer(1));
-	    } else {
-		ccnts.setElementAt(new Integer(((Integer)ccnts.elementAt(i)).intValue()+1),i);
-	    }
-	}
-	if (!Double.isNaN(d)) {
+            int i=cats.indexOf(oo);
+            if (i==-1) {
+                cats.addElement(oo);
+                ccnts.addElement(new Integer(1));
+            } else {
+                ccnts.setElementAt(new Integer(((Integer)ccnts.elementAt(i)).intValue()+1),i);
+            }
+        }
+        if (!Double.isNaN(d)) {
             if (d>max) max=d;
             if (d<min) min=d;
-	} else
+        } else
             missingCount++;
         cont[insertPos++]=d;
         NotifyAll(new NotifyMsg(this,Common.NM_VarContentChange));
-	return true;
+        return true;
     }
-
+    
     /** replaces an element at specified position - use with care!. this doesn't work for categorical variables.
-        in that case you need to dropCat(), make modifications and categorize().
-        also numerical variables only "grow" their min/max - i.e. if min/max was the removed
-        element, then min/max is not adapted to shrink the range
-        */
+     * in that case you need to dropCat(), make modifications and categorize().
+     * also numerical variables only "grow" their min/max - i.e. if min/max was the removed
+     * element, then min/max is not adapted to shrink the range
+     */
     public boolean replace(int i, Object o) {
         try {replace(i,Double.parseDouble(o.toString())); return true;} catch (Exception e) {}
         return false;
     }
-
+    
     public boolean replace(int i, double d) {
         if (i<0 || i>=cont.length || isCat()) return false;
         if (Double.isNaN(cont[i])) missingCount--;
@@ -250,24 +247,24 @@ public class SVarDouble extends SVar
         if (Double.isNaN(d)) missingCount++;
         return true;
     }
-
+    
     public Object at(int i) {
         return (i<0||i>=insertPos||Double.isNaN(cont[i]))?null:new Double(cont[i]);
     };
     public double atD(int i) { return (i<0||i>=insertPos)?double_NA:cont[i]; }
     public int atI(int i) { return (i<0||i>=insertPos||Double.isNaN(cont[i]))?int_NA:((int)(cont[i]+0.5)); }
     public String asS(int i) { return (i<0||i>=insertPos||Double.isNaN(cont[i]))?null:Double.toString(cont[i]); }
-
+    
     /** returns the ID of the category of the object
-        @param object
-        @return category ID
+     * @param object
+     * @return category ID
      */
     public int getCatIndex(Object o) {
-	if (cats==null) return -1;
+        if (cats==null) return -1;
         Object oo=o; if(o==null) oo=missingCat;
-	return cats.indexOf(oo);
+        return cats.indexOf(oo);
     }
-
+    
     /** returns ID of the category of i-th case in the variable or -1 if i oob */
     public int getCatIndex(int i) {
         try {
@@ -276,7 +273,7 @@ public class SVarDouble extends SVar
             return -1;
         }
     }
-
+    
     /** returns the category with index ID or <code>null</code> if variable is not categorial */
     public Object getCatAt(int i) {
         if (cats==null) return null;
@@ -286,39 +283,39 @@ public class SVarDouble extends SVar
             return null;
         }
     }
-
+    
     /** returns size of the category with index ID or -1 if variable is not categorial or index oob */
     public int getSizeCatAt(int i) {
-	if (cats==null) return -1;
+        if (cats==null) return -1;
         try { // catch exception if cat ID is out of bounds
             return ((Integer)ccnts.elementAt(i)).intValue();
         } catch  (Exception e) {
             return -1;
         }
     }
-
+    
     /** returns size of the category o. If category does not exist or variable is not categorial, -1 is returned. */
     public int getSizeCat(Object o) {
-	if (cats==null) return -1;
-	int i=cats.indexOf(o);
-	return (i==1)?-1:((Integer)ccnts.elementAt(i)).intValue();
+        if (cats==null) return -1;
+        int i=cats.indexOf(o);
+        return (i==1)?-1:((Integer)ccnts.elementAt(i)).intValue();
     }
-
+    
     /** returns the number of categories for this variable or 0 if the variable is not categorial */
     public int getNumCats() {
-	if (cats==null) return 0;
-	return cats.size();
+        if (cats==null) return 0;
+        return cats.size();
     }
-
+    
     /** returns new, ed array of categories */
     public Object[] getCategories() {
-	if (cats==null) return null;
-
-	Object c[] = new Object[cats.size()];
-	cats.copyInto(c);
-	return c;
+        if (cats==null) return null;
+        
+        Object c[] = new Object[cats.size()];
+        cats.copyInto(c);
+        return c;
     }
-
+    
     /** we don't support replace [[ME: replace needs to re-alloc the vector or something like that ... ]] */
     public boolean remove(int index) {
         int length = size();
@@ -331,13 +328,12 @@ public class SVarDouble extends SVar
             cont = temp;
             insertPos = cont.length;
             return true;
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             return false;
         }
     }
-
-
+    
+    
     public boolean insert(Object o, int index) {
         int length = size();
         temp = new double[++length];
@@ -350,34 +346,33 @@ public class SVarDouble extends SVar
             cont[index] = o==null?double_NA:Double.parseDouble(o.toString());
             insertPos = cont.length;
             return true;
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
     }
-
+    
     /** returns list of indexes ordered by rank, for non-cat, num vars only. missing
-        values are omitted.
-        @param v variable (should be obtained by at(..))
-        @param m marker to use for filtering. if <code>null</code> all cases will be checked
-        @param markspec mark for filtering, i.e. cases with that mark will be used only
-        @return list of indexes or <code>null</code> is any of the following
-cases: variable is not numerical or is categorical, no cases matching
-        specification are present */
+     * values are omitted.
+     * @param v variable (should be obtained by at(..))
+     * @param m marker to use for filtering. if <code>null</code> all cases will be checked
+     * @param markspec mark for filtering, i.e. cases with that mark will be used only
+     * @return list of indexes or <code>null</code> is any of the following
+     * cases: variable is not numerical or is categorical, no cases matching
+     * specification are present */
     public int[] getRanked(SMarker m, int markspec) {
         Stopwatch sw=new Stopwatch();
         if (isCat() || !isNum() || size()==0) return null;
-
+        
         if (m==null && cacheRanks && ranks!=null) return ranks; // we can cache only ranks w/o a marker
-
+        
         int[] r=null;
-
+        
         /* okay in fact we always get the full ranks and then pick those marked (if desired) */
-
+        
         if (!cacheRanks || ranks==null) {
             // due to the massive amount of lookups necessary during the sorting, we allocate a separate double buffer with a copy of the data and work on that one instead of the atD access, if the number of cases is large enough.
-
+            
             ProgressDlg pd=null;
             if (size()>1000) {
                 pd=new ProgressDlg("Variable "+getName());
@@ -386,39 +381,33 @@ cases: variable is not numerical or is categorical, no cases matching
             int ct=size();
             r = new int[ct];
             double[] da = cont;
-
+            
             sw.profile("getRanked: pass 1: store relevant values");
-
-            // pass 3: sort by value
-            int i=0;
-            while (i<ct-1) {
-                double d=da[r[i]];
-                int j=ct-1;
-                if (pd!=null && (i&255)==0)
-                    pd.setProgress((int)(((double)i)*99.0/((double)ct)));
-                while (j>i) {
-                    double d2=da[r[j]];
-                    if (d2<d) {
-                        int xx=r[i]; r[i]=r[j]; r[j]=xx;
-                        d=d2;
-                    }
-                    j--;
+            
+            // use Collections.sort to compute the ranks
+            class SortDouble implements Comparable{
+                double d;
+                int index;
+                public SortDouble(double d, int index){
+                    this.d=d;
+                    this.index=index;
                 }
-                i++;
+
+                public int compareTo(Object o) {
+                    double od = ((SortDouble)o).d;
+                    if(od>d) return -1;
+                    if(od<d) return 1;
+                    return 0;
+                }
             }
-            if (pd!=null)
-                pd.setProgress(99);
-            sw.profile("getRanked: pass 2: sort");
-            if (cacheRanks)
-                ranks=r;
-            da=null;
-            if (pd!=null)
-                pd.end();
-            pd=null;
+            Vector data = new Vector(da.length);
+            for(int i=0; i<da.length; i++) data.add(new SortDouble(da[i],i));
+            Collections.sort(data);
+            for(int i=0; i<da.length; i++) r[i] = ((SortDouble)data.elementAt(i)).index;
         } else {
             r=ranks;
         }
-
+        
         // we got the full list - now we need to thin it out if a marker was specified
         if (m!=null && r!=null) {
             int x=r.length;
@@ -441,11 +430,11 @@ cases: variable is not numerical or is categorical, no cases matching
             r=null;
             r=mr;
         }
-
+        
         // return the resulting list
         return r;
     }
-
+    
     public String toString() {
         return "SVarDouble(\""+name+"\","+(cat?"cat,":"cont,")+(isnum?"num,":"txt,")+"n="+size()+"/"+cont.length+",miss="+missingCount+")";
     }
