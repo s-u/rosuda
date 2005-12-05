9eba61d7a21d063ada3680a200043318f5ba9a68
Simon Urbanek <simon.urbanek@r-project.org>
2005-12-05 06:02:57 +0000

    Factory for creating PlotComponents and associated widgets automatically (such as QueryPopup).
    git-svn-id: svn://svn.rforge.net/org/trunk@1797 955e8658-2bef-402f-a597-8b5c18d8b76d

---
+++ rosuda/ibase/toolkit/PlotComponentFactory.java
@@ -0,0 +1,45 @@
+//
+//  PlotComponentFactory.java
+//  
+//  Factory for creating PlotComponents and associated widgets automatically
+//
+//  Created by Simon Urbanek on 12/4/05.
+//  $Id$
+//
+
+package org.rosuda.ibase.toolkit;
+
+import org.rosuda.ibase.*;
+import java.awt.Window;
+
+/** Factory for creating PlotComponents and associated widgets automatically. All methods are static, thus creating an instance of this class doesn't make any sense. */
+public class PlotComponentFactory {
+	/** create an empty PlotComponent using the specified engine (AWT/SWING/...) */
+	public static PlotComponent createPlotComponent(int engine) {
+		if (engine == PlotComponent.AWT)
+			return new AwtPlotComponent();
+		else if (engine == PlotComponent.SWING)
+			return new SwingPlotComponent();
+		else throw(new RuntimeException("PlotComponentFactory: unsupported plot component engine ("+engine+")"));
+	}
+	
+	/** create an empty PlotComponent using the default engine (see Common.defaultPlotComponentType) */
+	public static PlotComponent createPlotComponent() {
+		return createPlotComponent(Common.defaultPlotComponentType);
+	}
+
+	/** create a QueryPopup that fits the supplied PlotComponent. If the component has a parent window already, then the win argument can be null, otherwise win is used as the parent frame for the popup. */
+	public static QueryPopup createQueryPopup(PlotComponent pc, Window win, SVarSet vs, String ct, int w, int cid) {
+		int engine = pc.getGraphicsEngine();
+		if (engine == PlotComponent.AWT)
+			return new AwtQueryPopup(pc, win, vs, ct, w, cid);
+		else if (engine == PlotComponent.SWING)
+			return new SwingQueryPopup(pc, win, vs, ct, w, cid);
+		else throw(new RuntimeException("PlotComponentFactory.createQueryPopup: unsupported plot component engine ("+engine+")"));
+	}
+
+	/** create a QueryPopup that fits the supplied PlotComponent. If the component has a parent window already, then the win argument can be null, otherwise win is used as the parent frame for the popup. */
+	public static QueryPopup createQueryPopup(PlotComponent pc, Window win, SVarSet vs, String ct) {
+		return createQueryPopup(pc, win, vs, ct, -1, -1);
+	}
+}
