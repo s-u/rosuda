1fbfb94c3409bffbdccab8c7a8ae011d03b4ee96
Markus Helbig <info@markushelbig.de>
2006-02-21 12:43:53 +0000

    new version number
    git-svn-id: svn://svn.rforge.net/org/trunk@1977 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/JGR/JGR.java
+++ rosuda/JGR/JGR.java
@@ -37,7 +37,7 @@ public class JGR {
 	/* Copyright information and other stuff */
 
 	/** Version number of JGR */
-	public static final String VERSION = "1.3a";
+	public static final String VERSION = "1.4";
 
 	/** Title (used for displaying the splashscreen) */
 	public static final String TITLE = "JGR";
@@ -46,7 +46,7 @@ public class JGR {
 	public static final String SUBTITLE = "Java Gui for R";
 
 	/** Develtime (used for displaying the splashscreen) */
-	public static final String DEVELTIME = "2003 - 2005";
+	public static final String DEVELTIME = "2003 - 2006";
 
 	/** Organization (used for displaying the splashscreen) */
 	public static final String INSTITUTION = "RoSuDa, Univ. Augsburg";
@@ -397,8 +397,10 @@ public class JGR {
 		}
 		st = new StringTokenizer(previous,",");
 		previous = "";
-		while (st.hasMoreTokens()) 
-			previous += st.nextToken()+(st.hasMoreTokens()?",":"");
+		while (st.hasMoreTokens()) {
+			String prev = st.nextToken().trim();
+			if (prev != null && prev != "null") previous += st.nextToken()+(st.hasMoreTokens()?",":"");
+		}
 		if (previous.trim().length() > 0) new JGRPackageManager(previous);
 	}
 
