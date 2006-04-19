9d6d1cf8d8ea982956a9f0ce35fb38aa464aa440
Markus Helbig <info@markushelbig.de>
2006-04-19 19:35:46 +0000

    added constants to abstract level FrameDevice instead of TFrame and TJFrame (variables still there)
    git-svn-id: svn://svn.rforge.net/org/trunk@2255 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/ibase/toolkit/FrameDevice.java
+++ rosuda/ibase/toolkit/FrameDevice.java
@@ -5,6 +5,28 @@ import java.awt.event.*;
 
 public interface FrameDevice {
 
+    public static final int clsMain     = 1;
+    public static final int clsSplash   = 2;
+    public static final int clsVars     = 8;
+    public static final int clsHelp     = 16;
+    public static final int clsPlot     = 128;
+    public static final int clsTree     = 129;
+    public static final int clsMCP      = 130;
+    public static final int clsDevPlot  = 131;
+    public static final int clsTreeMap  = 132;
+
+    public static final int clsBar      = 135;
+    public static final int clsHist     = 136;
+    public static final int clsScatter  = 137;
+    public static final int clsBox      = 138;
+    public static final int clsPCP      = 139;
+    public static final int clsLine     = 140;
+    public static final int clsMap      = 141;
+    public static final int clsFD       = 142;
+    public static final int clsTable    = 143;
+
+    public static final int clsUser     = 8192;
+	
 	void initPlacement();
 	Frame getFrame();
 	
