46757be842a6071cc1f8edbdec4f3002476502f5
Markus Helbig <info@markushelbig.de>
2006-04-19 20:01:35 +0000

    moved static variables to FrameDevice
    git-svn-id: svn://svn.rforge.net/org/trunk@2257 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/ibase/toolkit/TJFrame.java
+++ rosuda/ibase/toolkit/TJFrame.java
@@ -12,28 +12,6 @@ public class TJFrame extends JFrame implements FrameDevice
 {
     WTentry WTmyself;
 
-    public static final int clsMain     = 1;
-    public static final int clsSplash   = 2;
-    public static final int clsVars     = 8;
-    public static final int clsHelp     = 16;
-    public static final int clsPlot     = 128;
-    public static final int clsTree     = 129;
-    public static final int clsMCP      = 130;
-    public static final int clsDevPlot  = 131;
-    public static final int clsTreeMap  = 132;
-
-    public static final int clsBar      = 135;
-    public static final int clsHist     = 136;
-    public static final int clsScatter  = 137;
-    public static final int clsBox      = 138;
-    public static final int clsPCP      = 139;
-    public static final int clsLine     = 140;
-    public static final int clsMap      = 141;
-    public static final int clsFD       = 142;
-    public static final int clsTable    = 143;
-
-    public static final int clsUser     = 8192;
-    
     public TJFrame(final String tit, final boolean useCommonBg, final int wclass) {
         if (useCommonBg) setBackground(Common.backgroundColor);
 	setTitle(tit);
