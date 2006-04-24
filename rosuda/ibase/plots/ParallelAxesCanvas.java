23a9f45d82ca651736768706360bf255b0b43029
Tobias Wichtrey <wichtrey@math.uni-augsburg.de>
2006-04-24 08:24:50 +0000

    add flag alterningLabels
    git-svn-id: svn://svn.rforge.net/org/trunk@2268 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/ibase/plots/ParallelAxesCanvas.java
+++ rosuda/ibase/plots/ParallelAxesCanvas.java
@@ -61,6 +61,7 @@ public class ParallelAxesCanvas extends BaseCanvas {
     private boolean useRegularPositioning=false;
     private int leftGap=7;
     private int rightGap=7;
+    public boolean alterningLabels = false;
     
     /**
      * variables fields
@@ -636,28 +637,44 @@ public class ParallelAxesCanvas extends BaseCanvas {
                     if (labs[i]!=null){
                         
                         if(orientation==0){
-                            final boolean bottom = (i&1)==0;
+                            final boolean bottom = alterningLabels?((i&1)==0):true;
                             int maxWidth=-1;
                             double xAlign=0.5;
                             double yAlign=0.5;
                             
+                            // determine maxWidth, xAlign and yAlign;
                             switch(type){
                                 case TYPE_BOX:
-                                    if(i==0){
-                                        if(valuePoss.length>1) maxWidth=valuePoss[1]-valuePoss[0]+posBoxwidth/2;
-                                    } else if (i==valuePoss.length-1){
-                                        if(i>0) maxWidth=valuePoss[i]-valuePoss[i-1]+posBoxwidth/2;
+                                    if(alterningLabels){
+                                        if(i==0){
+                                            if(valuePoss.length>1) maxWidth=valuePoss[1]-valuePoss[0]+posBoxwidth/2;
+                                        } else if (i==valuePoss.length-1){
+                                            if(i>0) maxWidth=valuePoss[i]-valuePoss[i-1]+posBoxwidth/2;
+                                        } else{
+                                            if(i+1<valuePoss.length && i-1>=0) maxWidth=valuePoss[i+1]-valuePoss[i-1];
+                                        }
                                     } else{
-                                        if(i+1<valuePoss.length && i-1>=0) maxWidth=valuePoss[i+1]-valuePoss[i-1];
+                                        maxWidth = posBoxwidth;
                                     }
                                     break;
                                 case TYPE_PCP:
-                                    if(i==0){
-                                        if(valuePoss.length>1) maxWidth=valuePoss[1]-valuePoss[0];
-                                    } else if (i==valuePoss.length-1){
-                                        if(i>0) maxWidth=valuePoss[i]-valuePoss[i-1];
-                                    } else{
-                                        if(i+1<valuePoss.length && i-1>=0) maxWidth=valuePoss[i+1]-valuePoss[i-1];
+                                    if(alterningLabels){
+                                        if(i==0){
+                                            if(valuePoss.length>1) maxWidth=valuePoss[1]-valuePoss[0];
+                                        } else if (i==valuePoss.length-1){
+                                            if(i>0) maxWidth=valuePoss[i]-valuePoss[i-1];
+                                        } else{
+                                            if(i+1<valuePoss.length && i-1>=0) maxWidth=valuePoss[i+1]-valuePoss[i-1];
+                                        }
+                                    } else {
+                                        if(i==0){
+                                            maxWidth=(valuePoss[1]-valuePoss[0])/2;
+                                        } else if (i==valuePoss.length-1){
+                                            maxWidth=(valuePoss[i]-valuePoss[i-1])/2;
+                                        } else{
+                                            maxWidth=(valuePoss[i+1]-valuePoss[i-1])/2;
+                                        }
+                                        
                                     }
                                     
                                     xAlign=(i==0)?0:
@@ -668,7 +685,7 @@ public class ParallelAxesCanvas extends BaseCanvas {
                             }
                             
                             labels.add(valuePoss[i],
-                                    bottom?(H-mBottom+2):(mTop-5),
+                                    (bottom||!alterningLabels)?(H-mBottom+2):(mTop-5),
                                     xAlign,
                                     yAlign,
                                     maxWidth,
@@ -692,7 +709,7 @@ public class ParallelAxesCanvas extends BaseCanvas {
                 while (xx<xv.getNumCats()) {
                     final int t=getAxCatPos(xx);
                     if(orientation==0){
-                        if((ax.getCatSeqIndex(xx)&1)==0) g.drawLine(t,b,t,b+2);
+                        if(!alterningLabels || (ax.getCatSeqIndex(xx)&1)==0) g.drawLine(t,b,t,b+2);
                         else g.drawLine(t,mTop,t,mTop-2);
                     } else{
                         g.drawLine(mLeft,t,mLeft-2,t);
