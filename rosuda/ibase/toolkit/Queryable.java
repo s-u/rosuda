70062d23308110ddfdda0b236ccd7144f968af0c
Tobias Wichtrey <wichtrey@math.uni-augsburg.de>
2006-05-04 15:05:35 +0000

    add getQueryText, getExtQueryText
    git-svn-id: svn://svn.rforge.net/org/trunk@2374 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/ibase/toolkit/Queryable.java
+++ rosuda/ibase/toolkit/Queryable.java
@@ -5,5 +5,14 @@ public interface Queryable {
 
     /** returns whether this object should respond to queries. */
     boolean isQueryable();
+    
+    /** checks whether this object contains the given point.*/
+    boolean contains(int x, int y);
+    
+    /** returns text for query */
+    String getQueryText();
+    
+    /** returns text for extended query */
+    String getExtQueryText();
 
 }
