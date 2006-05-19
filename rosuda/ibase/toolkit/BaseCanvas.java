ba25f04b36b6616936e6c1b4f14c2b3eab197653
Tobias Wichtrey <wichtrey@math.uni-augsburg.de>
2006-05-19 08:21:59 +0000

    rotate y labels
    git-svn-id: svn://svn.rforge.net/org/trunk@2418 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/ibase/toolkit/BaseCanvas.java
+++ rosuda/ibase/toolkit/BaseCanvas.java
@@ -1198,14 +1198,17 @@ public class BaseCanvas extends PGSCanvas implements Dependent, MouseListener, M
         try {
             final int maxW = abbreviate?(mLeft-2):(-1);
             final int xPos = mLeft-3;
-            final double xAlign = 1;
-            final double yALign = 0.4;
+            final double xAlign = 0.5;
+            final double yALign = 1;
             
             while (fi<axis.vBegin+axis.vLen) {
+                final String text;
+                if(sVar==null) text = axis.getDisplayableValue(fi);
+                else text=sVar.getCatAt((int)(fi+0.5)).toString();
+                
                 final int t=axis.getValuePos(fi);
+                labels.add(xPos,t,xAlign,yALign,maxW,90,text);
                 if(ticks) g.drawLine(mLeft-2,t,mLeft,t);
-                if(sVar==null) labels.add(xPos,t,xAlign,yALign,maxW,axis.getDisplayableValue(fi));
-                else labels.add(xPos,t,xAlign,yALign,maxW,sVar.getCatAt((int)(fi+0.5)).toString());
                 fi+=f;
             }
         } catch (Exception pae) { // catch problems (especially in getCatAt being 0)
