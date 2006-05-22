6ff6dd07a23bfa195229659e9022ace4456517f1
Martin Theus <martin@theusrus.de>
2006-05-22 12:56:24 +0000

    Drag drop open problem Mac ...
    git-svn-id: svn://svn.rforge.net/org/trunk@2430 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/Mondrian/Mondrian.java
+++ rosuda/Mondrian/Mondrian.java
@@ -1,9 +1,13 @@
-import java.util.Vector;         //
+import java.io.*;            
+import java.util.Vector;        
+import com.apple.mrj.*;
 
-public class Mondrian 
+
+public class Mondrian implements MRJOpenDocumentHandler
 {
 
   protected Vector dataSets = new Vector(5,5);
+  public Vector Mondrians = new Vector(5,5);
 
   /** A very simple main() method for our program. */
   public static void main(String[] args) {
@@ -11,8 +15,26 @@ public class Mondrian
   }
 
   public Mondrian() {
-    new Join(dataSets, false, false, null);
+
+    Join First = new Join(Mondrians, dataSets, false, false, null);
+
+//    System.out.println(" Join Created / Register Handler ...");
+    
+    MRJApplicationUtils.registerOpenDocumentHandler ( this );
+
+    try { 
+      // put it to sleep "forever" ... 
+      Thread.sleep(Integer.MAX_VALUE); 
+    } catch (InterruptedException e) { 
+    } 
   }
-
+  
+  public void handleOpenFile( File inFile )
+  {
+    Join theJoin = ((Join)Mondrians.lastElement());
+//    while( !theJoin.mondrianRunning ) {System.out.println(" wait for Mondrian to initialize ...");}   // Wait until Mondrian initialized
+//    System.out.println(".......... CALL loadDataSet("+inFile+") FROM handleOpenFile .........");
+    theJoin.loadDataSet( false, inFile );
+  }
+  
 }
-
