2686bfe73909b9441f5041a1db929907b4186f29
Simon Urbanek <simon.urbanek@r-project.org>
2006-11-30 18:41:07 +0000

    Fix a bug in MRJFileUtils (wrong signature)
    git-svn-id: svn://svn.rforge.net/org/trunk@2682 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/util/MRJstubs/MRJFileUtils.java
+++ rosuda/util/MRJstubs/MRJFileUtils.java
@@ -1,6 +1,8 @@
 package com.apple.mrj;                 
 
+import java.io.File;
+
 public class MRJFileUtils {
     /* this is the only one we use, forget about others */
-    public static String getResource(String resourceName) { return null; }
+    public static File getResource(String resourceName) { return null; }
 }
