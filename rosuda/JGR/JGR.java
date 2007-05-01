41c36bfb383ce26119ffcd59093b4aea005b03e4
Markus Helbig <info@markushelbig.de>
2007-05-01 20:27:57 +0000

    devel time
    git-svn-id: svn://svn.rforge.net/org/trunk@2738 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/JGR/JGR.java
+++ rosuda/JGR/JGR.java
@@ -44,10 +44,10 @@ import org.rosuda.util.Global;
 
 public class JGR {
 
-	//JGR_VERSION 1.4-15
+	//JGR_VERSION 1.4-16
 	
 	/** Version number of JGR */
-	public static final String VERSION = "1.4-15";
+	public static final String VERSION = "1.4-16";
 
 	/** Title (used for displaying the splashscreen) */
 	public static final String TITLE = "JGR";
@@ -56,7 +56,7 @@ public class JGR {
 	public static final String SUBTITLE = "Java Gui for R";
 
 	/** Develtime (used for displaying the splashscreen) */
-	public static final String DEVELTIME = "2003 - 2006";
+	public static final String DEVELTIME = "2003 - 2007";
 
 	/** Organization (used for displaying the splashscreen) */
 	public static final String INSTITUTION = "RoSuDa, Univ. Augsburg";
