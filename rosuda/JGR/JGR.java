b47b6b1789a65ed9585ad20e854090247ab44210
Markus Helbig <info@markushelbig.de>
2007-06-03 12:12:57 +0000

    new version number for JGR
    git-svn-id: svn://svn.rforge.net/org/trunk@2791 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/JGR/JGR.java
+++ rosuda/JGR/JGR.java
@@ -47,7 +47,7 @@ public class JGR {
 	//JGR_VERSION 1.4-16
 	
 	/** Version number of JGR */
-	public static final String VERSION = "1.4-18";
+	public static final String VERSION = "1.5-1";
 
 	/** Title (used for displaying the splashscreen) */
 	public static final String TITLE = "JGR";
