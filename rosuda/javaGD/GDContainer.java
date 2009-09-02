52cba012221c46b025a5cb1b73000995c6a58693
Simon Urbanek <simon.urbanek@r-project.org>
2009-09-02 13:37:24 +0000

    add docs
    git-svn-id: svn://svn.rforge.net/org/trunk@3166 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/javaGD/GDContainer.java
+++ rosuda/javaGD/GDContainer.java
@@ -3,7 +3,7 @@
 //  Java Graphics Device
 //
 //  Created by Simon Urbanek on Thu Aug 05 2004.
-//  Copyright (c) 2004 Simon Urbanek. All rights reserved.
+//  Copyright (c) 2004-2009 Simon Urbanek. All rights reserved.
 //
 //  This library is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU Lesser General Public
@@ -25,17 +25,36 @@ package org.rosuda.javaGD;
 
 import java.awt.*;
 
+/** <code>GDContainer</code> is the minimal interface that has to be implemented by classes that are used as back-ends for JavaGD. The interface feeds graphics objects to the instance which are then free to use them for any purpose such as display or printing. */
 public interface GDContainer {
+    /** add a new plot object to the list
+     *  @param o plot object */
     public void add(GDObject o);
+    /** reset the plot- remove all objects */
     public void reset();
+    /** retrieve graphics state
+     *  @return current graphics state */
     public GDState getGState();
+    /** retrieve graphics if this container is backed by some {@link Graphics} object.
+     *  @return current graphics object or <code>null</code> if this container is not associated with any */
     public Graphics getGraphics(); // implementation is free to return null
     //public void repaint();
     //public void repaint(long tm);
-    public boolean prepareLocator(LocatorSync ls); // this method is called to notify the contained that a locator request is pending; the container must either return @code{false} and ignore the ls parameter *or* return @code{true} and call @link{LocatorSync.triggerAction} method at some point in the future (which may well be after returning from this method)
-    public void syncDisplay(boolean finish); // true=batch finished; false=batch begins
+    /** this method is called to notify the contained that a locator request is pending; the container must either return <code>false</code> and ignore the <code>ls</code> parameter *or* return <code>true</code> and call @link{LocatorSync.triggerAction} method at some point in the future (which may well be after returning from this method)
+     * @param ls locator synchronization object */
+    public boolean prepareLocator(LocatorSync ls);
+    /** synchronize display with the graphics objects
+     *  @param finish flag denoting whether the synchronization is desired or not (<code>true</code> for a finished batch, <code>false</code> when a batch starts) */
+    public void syncDisplay(boolean finish);
+    /** set the device number of this container
+     *  @param dn device number */
     public void setDeviceNumber(int dn);
+    /** close the display associated with this container */
     public void closeDisplay();
+    /** retrieve the current device number
+     *  @return current device number */
     public int getDeviceNumber();
+    /** retrieve the size of the container
+     *  @return size of the container */
     public Dimension getSize();
 }
