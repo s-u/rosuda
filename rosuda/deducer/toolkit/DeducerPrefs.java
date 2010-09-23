6499c8cd4d3a8ceed10815b3859e8e29fe4ee9df
Ian Fellows <ian.fellows@stat.ucla.edu>
2010-09-23 23:54:57 +0000

    Deducer: allow plug-ins to add preference items
    git-svn-id: svn://svn.rforge.net/org/trunk@3338 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/deducer/toolkit/DeducerPrefs.java
+++ rosuda/deducer/toolkit/DeducerPrefs.java
@@ -7,6 +7,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.prefs.BackingStoreException;
 import java.util.prefs.InvalidPreferencesFormatException;
 import java.util.prefs.Preferences;
@@ -24,12 +25,20 @@ public class DeducerPrefs {
 	public static boolean USEQUAQUACHOOSER;
 	public static boolean VIEWERATSTARTUP;
 	
+	private static HashMap map;
+	
+	private static boolean started = false;
+	
 	public static void initialize() {
-		SHOWDATA = true;
-		SHOWANALYSIS = true;
-		USEQUAQUACHOOSER = Common.isMac() ? true : false;
-		VIEWERATSTARTUP = true;		
-		readPrefs();
+		if(!started){
+			SHOWDATA = true;
+			SHOWANALYSIS = true;
+			USEQUAQUACHOOSER = Common.isMac() ? true : false;
+			VIEWERATSTARTUP = true;		
+			map = new HashMap();
+			readPrefs();
+			started = true;
+		}
 	}
 	
 	public static void readPrefs() {
@@ -61,11 +70,25 @@ public class DeducerPrefs {
 			return;
 		}
 		Preferences prefs = Preferences.userNodeForPackage(org.rosuda.deducer.Deducer.class);
-		
 		SHOWDATA = prefs.getBoolean("SHOWDATA",true);
 		SHOWANALYSIS = prefs.getBoolean("SHOWANALYSIS",true);
 		USEQUAQUACHOOSER = prefs.getBoolean("USEQUAQUACHOOSER",Common.isMac() ? true : false);
 		VIEWERATSTARTUP = prefs.getBoolean("VIEWERATSTARTUP",true);
+		String[] names;
+		try {
+			names = prefs.keys();
+			System.out.println("names:"+names.length);
+			for(int i=0;i<names.length;i++){
+				if(!names[i].equals("SHOWDATA") && !names[i].equals("SHOWANALYSIS") &&
+					!names[i].equals("USEQUAQUACHOOSER") && !names[i].equals("VIEWERATSTARTUP")){
+					
+					map.put(names[i], prefs.get(names[i], ""));
+				}
+			}			
+		} catch (BackingStoreException e) {
+			e.printStackTrace();
+		}
+
 	}
 	
 	public static void writePrefs() {
@@ -75,6 +98,11 @@ public class DeducerPrefs {
 		try {
 			prefs.clear();
 		} catch (Exception x) {
+		}
+
+		Object[] keys = map.keySet().toArray();
+		for(int i=0;i<keys.length;i++){
+			prefs.put((String) keys[i], (String) map.get(keys[i]));
 		}
 		prefs.putBoolean("SHOWDATA", SHOWDATA);
 		prefs.putBoolean("SHOWANALYSIS", SHOWANALYSIS);
@@ -88,6 +116,33 @@ public class DeducerPrefs {
 		} catch (BackingStoreException e) {
 		}
 
+	}
+	
+	public static String get(String key){
+		if(key.equals("SHOWDATA"))
+			return SHOWDATA ? "true" : "false";
+		if(key.equals("SHOWANALYSIS"))
+			return SHOWANALYSIS ? "true" : "false";
+		if(key.equals("USEQUAQUACHOOSER"))
+			return USEQUAQUACHOOSER ? "true" : "false";
+		if(key.equals("VIEWERATSTARTUP"))
+			return VIEWERATSTARTUP ? "true" : "false";
+		
+		return (String) map.get(key);
+	}
+	
+	public static void set(String key, String value){
+		if(key.equals("SHOWDATA"))
+			SHOWDATA = value.equals("true");
+		else if(key.equals("SHOWANALYSIS"))
+			SHOWANALYSIS = value.equals("true");
+		else if(key.equals("USEQUAQUACHOOSER"))
+			USEQUAQUACHOOSER = value.equals("true");
+		else if(key.equals("VIEWERATSTARTUP"))
+			VIEWERATSTARTUP = value.equals("true");	
+		else{
+			map.put(key, value);
+		}
 	}
 	
 }
