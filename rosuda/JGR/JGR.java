469bb15db1d8d5d95f93c705010b4059a1fdcb75
Markus Helbig <info@markushelbig.de>
2011-05-22 20:30:01 +0000

    new dev version
    git-svn-id: svn://svn.rforge.net/org/trunk@3400 955e8658-2bef-402f-a597-8b5c18d8b76d

--- rosuda/JGR/JGR.java
+++ rosuda/JGR/JGR.java
@@ -59,7 +59,7 @@ import org.rosuda.util.Global;
 public class JGR {
 
 	/** Version number of JGR */
-	public static final String VERSION = "1.7-7";
+	public static final String VERSION = "1.7-8";
 
 	/** Title (used for displaying the splashscreen) */
 	public static final String TITLE = "JGR";
